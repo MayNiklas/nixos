f20c07dd5f3b5673b360b2497d8f307ca247fa44
MayNiklas <info@niklas-steffen.de>
2020-12-28 20:44:48 +0100

    Creating user

--- configuration.nix
+++ configuration.nix
@@ -79,10 +79,16 @@
   # services.xserver.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
-  # users.users.jane = {
-  #   isNormalUser = true;
-  #   extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-  # };
+  users.users.nik = {
+    isNormalUser = true;
+    home = "/home/nik";
+    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+    openssh.authorizedKeys.keyFiles = [
+      (builtins.fetchurl { url = "https://github.com/mayniklas.keys"; })
+    ];
+  };
+
+  nix.allowedUsers = [ "nik" ];
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
