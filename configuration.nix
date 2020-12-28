d012a77462d635435be009587e3c570606f3410a
MayNiklas <info@niklas-steffen.de>
2020-12-28 18:15:39 +0000

    Setting hostname & timezone

--- configuration.nix
+++ configuration.nix
@@ -31,11 +31,11 @@
     };
   };
 
-  # networking.hostName = "nixos"; # Define your hostname.
+  networking.hostName = "water-on-fire"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Set your time zone.
-  # time.timeZone = "Europe/Amsterdam";
+  time.timeZone = "Europe/Berlin";
 
   # The global useDHCP flag is deprecated, therefore explicitly set to false here.
   # Per-interface useDHCP will be mandatory in the future, so this generated config
