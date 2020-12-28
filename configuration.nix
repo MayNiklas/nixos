23f4b49fa09996856d9150c96ecc887fb92bf1af
MayNiklas <info@niklas-steffen.de>
2020-12-28 20:44:56 +0100

    Configuring ssh

--- configuration.nix
+++ configuration.nix
@@ -108,7 +108,12 @@
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+  services.openssh = {
+    enable = true;
+    passwordAuthentication = false;
+    startWhenNeeded = true;
+    challengeResponseAuthentication = false;
+  };
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
