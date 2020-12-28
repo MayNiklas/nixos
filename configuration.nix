ad9dc43b9718d0c22e73e157707afa177b493287
MayNiklas <info@niklas-steffen.de>
2020-12-28 20:44:41 +0100

    Enableing sound

--- configuration.nix
+++ configuration.nix
@@ -72,8 +72,8 @@
   # services.printing.enable = true;
 
   # Enable sound.
-  # sound.enable = true;
-  # hardware.pulseaudio.enable = true;
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
 
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
