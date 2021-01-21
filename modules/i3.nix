de76c160e84cd4134af789a22a335f1437244d6f
MayNiklas <info@niklas-steffen.de>
2021-01-21 14:41:38 +0100

    Removed i3 extra packages from system wide config

--- modules/i3.nix
+++ modules/i3.nix
@@ -8,22 +8,9 @@
       defaultSession = "none+i3";
       lightdm.enable = true;
     };
-
     windowManager.i3 = {
       enable = true;
       package = pkgs.i3-gaps;
-      extraPackages = with pkgs; [
-        dmenu
-        i3status
-        i3lock
-        i3blocks
-        arandr
-        rofi
-        xorg.xmodmap
-        xorg.xdpyinfo
-        pulsemixer
-        alacritty
-      ];
     };
   };
   nixpkgs = { config.allowUnfree = true; };
