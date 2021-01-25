4d3af02310c8299b475922ed31b6cd3538313abc
Niklas <44636701+MayNiklas@users.noreply.github.com>
2021-01-25 13:33:27 +0100

    Removed emulatedSystems = [ "aarch64-linux" ];
    System wasn't able to rebuild with channel unstable

--- modules/virtualization.nix
+++ modules/virtualization.nix
@@ -1,7 +1,5 @@
 { config, pkgs, lib, ... }: {
 
-  boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
-
   environment.systemPackages = with pkgs; [ docker-compose ];
 
   virtualisation.docker.enable = true;
