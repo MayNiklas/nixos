e66bbaefff87f553174d6300b74690c94ff9f634
Niklas <44636701+MayNiklas@users.noreply.github.com>
2021-01-26 17:29:49 +0100

    ↗️ Including screen setup in xserver.nix
    - ForceCompositionPipeline -> removes tearing
    - DP-0: main ultrawide 120Hz screen with G-Sync
    - DP-2: reference screen for color accuracy

--- modules/xserver.nix
+++ modules/xserver.nix
@@ -22,6 +22,12 @@
       EndSection
     '';
 
+    screenSection = ''
+      Option         "metamodes" "DP-0: 3440x1440_120 +1920+0 {ForceCompositionPipeline=On, ForceFullCompositionPipeline=On, AllowGSYNCCompatible=On}, DP-2: 1920x1200_60 +0+240 {ForceCompositionPipeline=On, ForceFullCompositionPipeline=On}"
+      Option         "AllowIndirectGLXProtocol" "off"
+      Option         "TripleBuffer" "on"
+    '';
+
     desktopManager = {
       xterm.enable = false;
       session = [{
