253b965e67fc206fec2cd486b4df7db1f25b8a85
Niklas <44636701+MayNiklas@users.noreply.github.com>
2021-02-07 19:10:03 +0100

    ↗️ Enabled libvirtd

--- modules/virtualization.nix
+++ modules/virtualization.nix
@@ -1,7 +1,10 @@
 { config, pkgs, lib, ... }: {
 
-  environment.systemPackages = with pkgs; [ docker-compose ];
+  environment.systemPackages = with pkgs; [ docker-compose virt-manager ];
 
   virtualisation.docker.enable = true;
+
+  virtualisation.libvirtd.enable = true;
+  programs.dconf.enable = true;
 
 }
