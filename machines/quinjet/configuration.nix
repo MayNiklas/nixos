878490e7bb62191d953ef72e951f0ab4a9255260
Niklas <44636701+MayNiklas@users.noreply.github.com>
2021-02-13 14:17:26 +0100

    ↗️ Added server quinjet: configuration

---
+++ machines/quinjet/configuration.nix
@@ -0,0 +1,61 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+
+    # Users
+    ../../users/nik.nix
+    ../../users/root.nix
+
+    # Modules
+    ../../modules/networking.nix
+    ../../modules/locale.nix
+    ../../modules/hosts.nix
+    ../../modules/openssh.nix
+    ../../modules/options.nix
+    ../../modules/plex.nix
+  ];
+
+  mainUser = "nik";
+  mainUserHome = "${config.users.extraUsers.${config.mainUser}.home}";
+
+  networking = {
+    hostName = "quinjet";
+  };
+
+  boot = {
+    loader = {
+      grub = {
+        enable = true;
+        version = 2;
+        device = "nodev";
+        efiSupport = true;
+      };
+      efi.canTouchEfiVariables = true;
+    };
+    cleanTmpDir = true;
+  };
+
+  environment.systemPackages = with pkgs; [
+    bash-completion
+    git
+    nixfmt
+    wget
+    htop
+  ];
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "20.09"; # Did you read the comment?
+
+}
