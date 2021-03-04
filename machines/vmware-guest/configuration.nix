1f0fd373b9e2fcf7d198f38bff50986e8e3a6dfc
Niklas <44636701+MayNiklas@users.noreply.github.com>
2021-03-04 17:22:03 +0100

    ↗️ Added machine vmware-guest as template (#6)
    * Added generated vmware-guest config
    * Intigrated vmware-guest into excisting structure

---
+++ machines/vmware-guest/configuration.nix
@@ -0,0 +1,46 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+
+    # Users
+    ../../users/nik.nix
+    ../../users/root.nix
+
+    # Modules
+    ../../modules/grub.nix
+    ../../modules/locale.nix
+    ../../modules/openssh.nix
+  ];
+
+  networking = {
+    hostName = "nixos-vm";
+    useDHCP = false;
+    interfaces.ens192.useDHCP = true;
+  };
+
+  environment.systemPackages = with pkgs; [
+    bash-completion
+    git
+    nixfmt
+    wget
+    htop
+  ];
+
+  virtualisation.vmware.guest.enable = true;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "20.09"; # Did you read the comment?
+
+}
