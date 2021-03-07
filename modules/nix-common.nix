a7c5f2b762519b4c8953f97d325b0a7ff170746d
Niklas <44636701+MayNiklas@users.noreply.github.com>
2021-03-07 15:34:30 +0100

    ↗️ create nix-common -> saving storage

---
+++ modules/nix-common.nix
@@ -0,0 +1,18 @@
+{ config, pkgs, ... }: {
+
+  nix = {
+
+    # Save space by hardlinking store files
+    autoOptimiseStore = true;
+
+    # Clean up old generations after 30 days
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 30d";
+    };
+
+    # Users allowed to run nix
+    allowedUsers = [ "root" ];
+  };
+}
