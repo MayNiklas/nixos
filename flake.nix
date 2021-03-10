d9c09e9be37ef531910139e7cffa1b1ddb5a9f1a
MayNiklas <info@niklas-steffen.de>
2021-03-10 20:33:45 +0100

    ↗️ flakes: adding default modules and systems

--- flake.nix
+++ flake.nix
@@ -11,11 +11,98 @@
     nixos-home.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, home-manager, nixos-home }: {
+  outputs = { self, nixpkgs, home-manager, nixos-home }:
+    let
+      # Function to create defult (common) system config options
+      defFlakeSystem = baseCfg:
+        nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            # Add home-manager option to all configs
+            ({ ... }: {
+              imports = [
+                {
+                  # Set the $NIX_PATH entry for nixpkgs. This is necessary in
+                  # this setup with flakes, otherwise commands like `nix-shell
+                  # -p pkgs.htop` will keep using an old version of nixpkgs.
+                  # With this entry in $NIX_PATH it is possible (and
+                  # recommended) to remove the `nixos` channel for both users
+                  # and root e.g. `nix-channel --remove nixos`. `nix-channel
+                  # --list` should be empty for all users afterwards
+                  nix.nixPath = [ "nixpkgs=${nixpkgs}" ];
+                }
+                baseCfg
+                home-manager.nixosModules.home-manager
+                # DONT set useGlobalPackages! It's not necessary in newer
+                # home-manager versions and does not work with configs using
+                # `nixpkgs.config`
+                { home-manager.useUserPackages = true; }
+              ];
+              # Let 'nixos-version --json' know the Git revision of this flake.
+              system.configurationRevision =
+                nixpkgs.lib.mkIf (self ? rev) self.rev;
+              nix.registry.nixpkgs.flake = nixpkgs;
+            })
+          ];
+        };
 
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+      base-modules-server = [
+        ./users/nik.nix
+        { home-manager.users.nik = nixos-home.nixosModules.server; }
+        ../../modules/docker.nix
+        ../../modules/grub.nix
+        ../../modules/nix-common.nix
+        ../../modules/networking.nix
+        ../../modules/locale.nix
+        ../../modules/hosts.nix
+        ../../modules/openssh.nix
+        ../../modules/options.nix
+        ../../modules/zsh.nix
+      ];
 
-    defaultPackage.x86_64-linux = self.packages.x86_64-linux.hello;
+      base-modules-desktop = [
+        ./users/nik.nix
+        { home-manager.users.nik = nixos-home.nixosModules.desktop; }
+        ../../modules/bluetooth.nix
+        ../../modules/locale.nix
+        ../../modules/networking.nix
+        ../../modules/nix-common.nix
+        ../../modules/openssh.nix
+        ../../modules/options.nix
+        ../../modules/hosts.nix
+        ../../modules/sound.nix
+        ../../modules/docker.nix
+        ../../modules/xserver.nix
+        ../../modules/yubikey.nix
+        ../../modules/zsh.nix
+      ];
+    in {
 
-  };
+      nixosConfigurations = {
+
+        water-on-fire = defFlakeSystem {
+          imports = base-modules-desktop ++ [
+            # Machine specific config
+            ./machines/water-on-fire/configuration.nix
+            ./machines/water-on-fire/hardware-configuration.nix
+          ];
+        };
+
+        quinjet = defFlakeSystem {
+          imports = base-modules-server ++ [
+            # Machine specific config
+            ./machines/quinjet/configuration.nix
+            ./machines/quinjet/hardware-configuration.nix
+          ];
+        };
+
+        the-bus = defFlakeSystem {
+          imports = base-modules-server ++ [
+            # Machine specific config
+            ./machines/the-bus/configuration.nix
+            ./machines/the-bus/hardware-configuration.nix
+          ];
+        };
+      };
+    };
 }
