43199dc7278da90fbdb719b147e4faaead7dfa8f
Niklas <44636701+MayNiklas@users.noreply.github.com>
2021-03-10 17:04:31 +0100

    ↗️ Changed names of display port outputs

--- modules/xserver.nix
+++ modules/xserver.nix
@@ -22,7 +22,7 @@
     '';
 
     screenSection = ''
-      Option         "metamodes" "DP-0: 3440x1440_120 +1920+0 {ForceCompositionPipeline=On, ForceFullCompositionPipeline=On, AllowGSYNCCompatible=On}, DP-2: 1920x1200_60 +0+240 {ForceCompositionPipeline=On, ForceFullCompositionPipeline=On}"
+      Option         "metamodes" "DP-2: 3440x1440_120 +1920+0 {ForceCompositionPipeline=On, ForceFullCompositionPipeline=On, AllowGSYNCCompatible=On}, DP-1: 1920x1200_60 +0+240 {ForceCompositionPipeline=On, ForceFullCompositionPipeline=On}"
       Option         "AllowIndirectGLXProtocol" "off"
       Option         "TripleBuffer" "on"
     '';
