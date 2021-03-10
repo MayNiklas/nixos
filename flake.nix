00fbf5b047e3bca0ef24d0070792010d2c79165b
MayNiklas <info@niklas-steffen.de>
2021-03-10 20:05:01 +0100

    ↗️ flakes inputs

--- flake.nix
+++ flake.nix
@@ -1,7 +1,17 @@
 {
   description = "A very basic flake";
 
-  outputs = { self, nixpkgs }: {
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    nixos-home.url = "github:mayniklas/nixos-home";
+    nixos-home.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, home-manager, nixos-home }: {
 
     packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
 
