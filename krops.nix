0fb273b620d77b3e15f3ca89eb6e18036214a431
MayNiklas <info@niklas-steffen.de>
2021-03-10 19:40:06 +0100

    ↗️ krops.nix: introduced all & restructured file

--- krops.nix
+++ krops.nix
@@ -1,4 +1,8 @@
+# Full example:
+# https://tech.ingolf-wagner.de/nixos/krops/
+
 let
+
   krops = (import <nixpkgs> { }).fetchgit {
     url = "https://cgit.krebsco.de/krops/";
     rev = "v1.17.0";
@@ -16,36 +20,35 @@ let
       # Copy repository to /var/src
       machine-config.file = toString ./.;
     }];
-in {
 
-  localhost = pkgs.krops.writeDeploy "deploy-localhost" {
-    source = source;
-    # You can use arbitrary targets here, as long as SSH is correctly
-    # configured
-    target = "root@localhost";
-  };
-
-  # nix-build ./krops.nix -A water-on-fire && ./result -j16
   water-on-fire = pkgs.krops.writeDeploy "deploy-water-on-fire" {
     source = source "water-on-fire";
     target = "root@water-on-fire";
   };
 
-  # nix-build ./krops.nix -A quinjet && ./result -j8
   quinjet = pkgs.krops.writeDeploy "deploy-quinjet" {
     source = source "quinjet";
     target = "root@quinjet";
   };
 
-  # nix-build ./krops.nix -A the-bus && ./result -j4
   the-bus = pkgs.krops.writeDeploy "deploy-the-bus" {
     source = source "the-bus";
     target = "root@the-bus";
   };
 
-  # Full example:
-  # https://tech.ingolf-wagner.de/nixos/krops/
+in {
 
-  # Deploy with (e.g. for server01:
-  # nix-build ./krops.nix -A server01 && ./result
+  # nix-build ./krops.nix -A water-on-fire && ./result -j16
+  water-on-fire = water-on-fire;
+
+  # nix-build ./krops.nix -A quinjet && ./result -j8
+  quinjet = quinjet;
+
+  # nix-build ./krops.nix -A the-bus && ./result -j4
+  the-bus = the-bus;
+
+  # nix-build ./krops.nix -A all && ./result -j16
+  all = pkgs.writeScript "deploy-all-servers"
+    (lib.concatStringsSep "\n" [ water-on-fire quinjet ]);
+
 }
