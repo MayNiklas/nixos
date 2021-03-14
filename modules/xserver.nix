c826160399cfb9e383980d919dad24f6f04e3240
MayNiklas <info@niklas-steffen.de>
2021-03-14 20:10:20 +0100

    ↗️ move screen config into seperate module

--- modules/xserver.nix
+++ modules/xserver.nix
@@ -21,12 +21,6 @@
       EndSection
     '';
 
-    screenSection = ''
-      Option         "metamodes" "DP-2: 3440x1440_120 +1920+0 {ForceCompositionPipeline=On, ForceFullCompositionPipeline=On, AllowGSYNCCompatible=On}, DP-1: 1920x1200_60 +0+240 {ForceCompositionPipeline=On, ForceFullCompositionPipeline=On}"
-      Option         "AllowIndirectGLXProtocol" "off"
-      Option         "TripleBuffer" "on"
-    '';
-
     desktopManager = {
       xterm.enable = false;
       session = [{
