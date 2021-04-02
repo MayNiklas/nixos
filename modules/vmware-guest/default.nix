e09d9bff92695c0f7d1fe41ad64ed5bb4a83445a
MayNiklas <info@niklas-steffen.de>
2021-04-02 21:22:27 +0200

    Change structure of vmware-guest module

--- modules/vmware-guest/default.nix
+++ modules/vmware-guest/default.nix
@@ -1,25 +1,35 @@
-{ config, pkgs, lib, ... }: {
+{ lib, pkgs, config, ... }:
+with lib;
+let cfg = config.mayniklas.vmware-guest;
+in {
 
-  fileSystems."/" = {
-    device = "/dev/disk/by-label/nixos";
-    autoResize = true;
-    fsType = "ext4";
+  options.mayniklas.vmware-guest = {
+    enable = mkEnableOption "activate vmware-guest" // { default = true; };
   };
 
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-label/ESP";
-    fsType = "vfat";
+  config = mkIf cfg.enable {
+
+    fileSystems."/" = {
+      device = "/dev/disk/by-label/nixos";
+      autoResize = true;
+      fsType = "ext4";
+    };
+
+    fileSystems."/boot" = {
+      device = "/dev/disk/by-label/ESP";
+      fsType = "vfat";
+    };
+
+    boot.growPartition = true;
+
+    boot.loader.grub = {
+      version = 2;
+      device = "nodev";
+      efiSupport = true;
+      efiInstallAsRemovable = true;
+    };
+
+    virtualisation.vmware.guest.enable = true;
+
   };
-
-  boot.growPartition = true;
-
-  boot.loader.grub = {
-    version = 2;
-    device = "nodev";
-    efiSupport = true;
-    efiInstallAsRemovable = true;
-  };
-
-  virtualisation.vmware.guest.enable = true;
-
 }
