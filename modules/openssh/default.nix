00b46418c054ca11f6fb50bae0611222e9ce42bb
MayNiklas <info@niklas-steffen.de>
2021-04-02 21:08:25 +0200

    Change structure of openssh module

--- modules/openssh/default.nix
+++ modules/openssh/default.nix
@@ -1,9 +1,22 @@
-{ config, pkgs, lib, ... }: {
-  # Enable the OpenSSH daemon.
-  services.openssh = {
-    enable = true;
-    passwordAuthentication = false;
-    startWhenNeeded = true;
-    challengeResponseAuthentication = false;
+{ lib, pkgs, config, ... }:
+with lib;
+let cfg = config.mayniklas.openssh;
+in {
+
+  options.mayniklas.openssh = {
+    enable = mkEnableOption "activate openssh" // { default = true; };
+  };
+
+  config = mkIf cfg.enable {
+
+    # Enable the OpenSSH daemon.
+    services.openssh = {
+      enable = true;
+      passwordAuthentication = false;
+      startWhenNeeded = true;
+      challengeResponseAuthentication = false;
+
+    };
+
   };
 }
