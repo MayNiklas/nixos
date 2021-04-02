158e34a18748d02836f3349308455d618bfed6ca
MayNiklas <info@niklas-steffen.de>
2021-04-02 21:04:27 +0200

    Change structure of networking module

--- modules/networking/default.nix
+++ modules/networking/default.nix
@@ -1,16 +1,26 @@
-{ config, pkgs, lib, ... }: {
+{ lib, pkgs, config, ... }:
+with lib;
+let cfg = config.mayniklas.networking;
+in {
 
-  networking = {
-
-    # Define the DNS servers
-    nameservers = [ "192.168.5.10" "192.168.5.1" "1.1.1.1" ];
-
-    # Enable networkmanager
-    networkmanager.enable = true;
-
-    # Open ports in the firewall.
-    # networking.firewall.allowedTCPPorts = [ ... ];
-    # networking.firewall.allowedUDPPorts = [ ... ];
+  options.mayniklas.networking = {
+    enable = mkEnableOption "activate networking" // { default = true; };
   };
-  users.extraUsers.${config.mainUser}.extraGroups = [ "networkmanager" ];
+
+  config = mkIf cfg.enable {
+
+    networking = {
+
+      # Define the DNS servers
+      nameservers = [ "192.168.5.10" "192.168.5.1" "1.1.1.1" ];
+
+      # Enable networkmanager
+      networkmanager.enable = true;
+
+      # Open ports in the firewall.
+      # networking.firewall.allowedTCPPorts = [ ... ];
+      # networking.firewall.allowedUDPPorts = [ ... ];
+    };
+    users.extraUsers.${config.mainUser}.extraGroups = [ "networkmanager" ];
+  };
 }
