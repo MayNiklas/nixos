82699d34136c57c89dbd79d3221f3529a5313e76
MayNiklas <info@niklas-steffen.de>
2021-04-02 21:09:35 +0200

    Change structure of plex module

--- modules/plex/default.nix
+++ modules/plex/default.nix
@@ -1,32 +1,44 @@
-{ config, pkgs, lib, ... }: {
-  services = {
-    plex = {
-      enable = true;
-      package = pkgs.plex;
-      openFirewall = true;
-      dataDir = "/var/lib/plex";
+{ lib, pkgs, config, ... }:
+with lib;
+let cfg = config.mayniklas.plex;
+in {
+
+  options.mayniklas.plex = {
+    enable = mkEnableOption "activate plex" // { default = true; };
+  };
+
+  config = mkIf cfg.enable {
+    services = {
+      plex = {
+        enable = true;
+        package = pkgs.plex;
+        openFirewall = true;
+        dataDir = "/var/lib/plex";
+      };
+      tautulli = {
+        enable = true;
+        package = pkgs.tautulli;
+        port = 8181;
+        dataDir = "/var/lib/plexpy";
+      };
     };
-    tautulli = {
-      enable = true;
-      package = pkgs.tautulli;
-      port = 8181;
-      dataDir = "/var/lib/plexpy";
+    networking.firewall.allowedTCPPorts = [ 8181 ];
+    fileSystems."/mnt/plex-media" = {
+      device = "${config.nasIP}:/volume1/plex-media";
+      options = [ "nolock" "soft" "ro" ];
+      fsType = "nfs";
     };
-  };
-  networking.firewall.allowedTCPPorts = [ 8181 ];
-  fileSystems."/mnt/plex-media" = {
-    device = "${config.nasIP}:/volume1/plex-media";
-    options = [ "nolock" "soft" "ro" ];
-    fsType = "nfs";
-  };
-  fileSystems."/mnt/media" = {
-    device = "${config.nasIP}:/volume1/media";
-    options = [ "nolock" "soft" "ro" ];
-    fsType = "nfs";
-  };
-  systemd.services.plex = { after = [ "mnt-media.mount mnt-plex\x2dmedia.mount" ]; };
-  nixpkgs = {
-    config.allowUnfreePredicate = pkg:
-      builtins.elem (lib.getName pkg) [ "plexmediaserver" ];
+    fileSystems."/mnt/media" = {
+      device = "${config.nasIP}:/volume1/media";
+      options = [ "nolock" "soft" "ro" ];
+      fsType = "nfs";
+    };
+    systemd.services.plex = {
+      after = [ "mnt-media.mount mnt-plexx2dmedia.mount" ];
+    };
+    nixpkgs = {
+      config.allowUnfreePredicate = pkg:
+        builtins.elem (lib.getName pkg) [ "plexmediaserver" ];
+    };
   };
 }
