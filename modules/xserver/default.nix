f73e2241fc453049178108ed149c81053f77db15
Niklas <44636701+MayNiklas@users.noreply.github.com>
2021-04-02 23:57:29 +0200

    Change structure of xserver module

--- modules/xserver/default.nix
+++ modules/xserver/default.nix
@@ -1,37 +1,50 @@
-{ config, pkgs, lib, ... }: {
-  # Enable the X11 windowing system.
-  services.xserver = {
-    layout = "de";
-    xkbOptions = "eurosign:e";
-    enable = true;
-    autorun = true;
-    dpi = 125;
-    libinput = {
+{ lib, pkgs, config, ... }:
+with lib;
+let cfg = config.mayniklas.xserver;
+in {
+
+  options.mayniklas.xserver = {
+    enable = mkEnableOption "activate xserver" // { default = true; };
+  };
+
+  config = mkIf cfg.enable {
+
+    # Enable the X11 windowing system.
+    services.xserver = {
+      layout = "de";
+      xkbOptions = "eurosign:e";
       enable = true;
-      touchpad.accelProfile = "flat";
+      autorun = true;
+      dpi = 125;
+      libinput = {
+        enable = true;
+        touchpad.accelProfile = "flat";
+      };
+
+      config = ''
+        Section "InputClass"
+          Identifier "mouse accel"
+          Driver "libinput"
+          MatchIsPointer "on"
+          Option "AccelProfile" "flat"
+          Option "AccelSpeed" "0"
+        EndSection
+      '';
+
+      desktopManager = {
+        xterm.enable = false;
+        session = [{
+          name = "home-manager";
+          start = ''
+            ${pkgs.runtimeShell} $HOME/.hm-xsession &
+             waitPID=$!
+          '';
+        }];
+      };
+
+      displayManager.lightdm.enable = true;
+    
     };
 
-    config = ''
-      Section "InputClass"
-        Identifier "mouse accel"
-        Driver "libinput"
-        MatchIsPointer "on"
-        Option "AccelProfile" "flat"
-        Option "AccelSpeed" "0"
-      EndSection
-    '';
-
-    desktopManager = {
-      xterm.enable = false;
-      session = [{
-        name = "home-manager";
-        start = ''
-          ${pkgs.runtimeShell} $HOME/.hm-xsession &
-           waitPID=$!
-        '';
-      }];
-    };
-
-    displayManager.lightdm.enable = true;
   };
 }
