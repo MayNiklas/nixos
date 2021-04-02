4cbf420f0f48083e3dcd3dea6985645d5156b2a9
MayNiklas <info@niklas-steffen.de>
2021-04-02 21:04:20 +0200

    Change structure of kde module

--- modules/kde/default.nix
+++ modules/kde/default.nix
@@ -1,10 +1,21 @@
-{ config, pkgs, lib, ... }: {
-  # Enable the Plasma 5 Desktop Environment.
-  services.xserver = {
-    enable = true;
-    displayManager.sddm.enable = true;
-    desktopManager.plasma5.enable = true;
-    layout = "de";
-    xkbOptions = "eurosign:e";
+{ lib, pkgs, config, ... }:
+with lib;
+let cfg = config.mayniklas.kde;
+in {
+
+  options.mayniklas.kde = {
+    enable = mkEnableOption "activate kde" // { default = true; };
+  };
+
+  config = mkIf cfg.enable {
+
+    # Enable the Plasma 5 Desktop Environment.
+    services.xserver = {
+      enable = true;
+      displayManager.sddm.enable = true;
+      desktopManager.plasma5.enable = true;
+      layout = "de";
+      xkbOptions = "eurosign:e";
+    };
   };
 }
