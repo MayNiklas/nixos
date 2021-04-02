ffbbf30b1055e21563379a2f963de1e8048548e3
MayNiklas <info@niklas-steffen.de>
2021-04-02 21:15:23 +0200

    Change structure of zsh module

--- modules/zsh/default.nix
+++ modules/zsh/default.nix
@@ -1,20 +1,30 @@
-{ config, pkgs, lib, ... }: {
+{ lib, pkgs, config, ... }:
+with lib;
+let cfg = config.mayniklas.zsh;
+in {
 
-  users.defaultUserShell = pkgs.zsh;
-
-  environment.systemPackages = with pkgs; [ zsh ];
-
-  # Needed for yubikey to work
-  environment.shellInit = ''
-    export ZDOTDIR=$HOME/.config/zsh
-  '';
-
-  programs.zsh = {
-    enable = true;
-    enableCompletion = true;
-    autosuggestions.enable = true;
+  options.mayniklas.zsh = {
+    enable = mkEnableOption "activate zsh" // { default = true; };
   };
 
-  # Needed for zsh completion of system packages, e.g. systemd
-  environment.pathsToLink = [ "/share/zsh" ];
+  config = mkIf cfg.enable {
+
+    users.defaultUserShell = pkgs.zsh;
+
+    environment.systemPackages = with pkgs; [ zsh ];
+
+    # Needed for yubikey to work
+    environment.shellInit = ''
+      export ZDOTDIR=$HOME/.config/zsh
+    '';
+
+    programs.zsh = {
+      enable = true;
+      enableCompletion = true;
+      autosuggestions.enable = true;
+    };
+
+    # Needed for zsh completion of system packages, e.g. systemd
+    environment.pathsToLink = [ "/share/zsh" ];
+  };
 }
