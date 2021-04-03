5c5e242292e970c6423d0b911911ec897a67257e
MayNiklas <info@niklas-steffen.de>
2021-04-03 23:13:32 +0200

    Change structure of in-stock-bot module

--- modules/containers/in-stock.nix
+++ modules/containers/in-stock.nix
@@ -1,8 +1,20 @@
-{ config, pkgs, ... }: {
-  virtualisation.oci-containers.containers.in-stock-bot = {
-    autoStart = true;
-    image = "mayniki/in-stock-bot";
-    extraOptions = [ "--env-file=/docker/in-stock-bot/envfile" ];
-    volumes = [ "/docker/in-stock-bot/data:/app/data:rw" ];
+{ lib, pkgs, config, ... }:
+with lib;
+let cfg = config.mayniklas.in-stock-bot;
+in {
+
+  options.mayniklas.in-stock-bot = {
+    enable = mkEnableOption "activate in-stock-bot" // { default = true; };
+  };
+
+  config = mkIf cfg.enable {
+
+    virtualisation.oci-containers.containers.in-stock-bot = {
+      autoStart = true;
+      image = "mayniki/in-stock-bot";
+      extraOptions = [ "--env-file=/docker/in-stock-bot/envfile" ];
+      volumes = [ "/docker/in-stock-bot/data:/app/data:rw" ];
+    };
+
   };
 }
