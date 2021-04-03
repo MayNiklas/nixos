4081e5ff5a1fbb90310a13348f860132bad23e75
Niklas <44636701+MayNiklas@users.noreply.github.com>
2021-04-04 00:04:08 +0200

    Add module pihole

---
+++ modules/pihole/default.nix
@@ -0,0 +1,26 @@
+{ lib, pkgs, config, ... }:
+with lib;
+let cfg = config.mayniklas.pihole;
+in {
+
+  options.mayniklas.pihole = {
+    enable = mkEnableOption "activate pihole" // { default = true; };
+  };
+
+  config = mkIf cfg.enable {
+
+    virtualisation.oci-containers.containers.pihole = {
+      autoStart = true;
+      image = "pihole/pihole:latest";
+      environment = {
+        TZ = "Europe/Berlin";
+      };
+      ports = [ "53:53/tcp" "53:53/udp" "67:67/udp" "80:80/tcp" "443:443/tcp" ];
+      volumes = [
+        "/docker/pihole/etc-pihole:/etc/pihole:rw"
+        "/docker/pihole/etc-dnsmasq.d:/etc/dnsmasq.d:rw"
+      ];
+    };
+
+  };
+}
