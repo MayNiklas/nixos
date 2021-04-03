329c91554e84bef219f39fa5a40110080aa5580e
MayNiklas <info@niklas-steffen.de>
2021-04-03 23:10:46 +0200

    Change structure of screen-config module

--- modules/screen-configs/CS2420-AW3420.nix
+++ modules/screen-configs/CS2420-AW3420.nix
@@ -1,9 +1,22 @@
-{ config, pkgs, lib, ... }: {
-  services.xserver = {
-    screenSection = ''
-      Option         "metamodes" "DP-2: 3440x1440_120 +1920+0 {ForceCompositionPipeline=On, ForceFullCompositionPipeline=On, AllowGSYNCCompatible=On}, DP-0: 1920x1200_60 +0+240 {ForceCompositionPipeline=On, ForceFullCompositionPipeline=On}"
-      Option         "AllowIndirectGLXProtocol" "off"
-      Option         "TripleBuffer" "on"
-    '';
+{ lib, pkgs, config, ... }:
+with lib;
+let cfg = config.mayniklas.eizo-alienware;
+in {
+
+  options.mayniklas.eizo-alienware = {
+    enable = mkEnableOption "eizo-alienware" // { default = true; };
+  };
+
+  config = mkIf cfg.enable {
+
+    services.xserver = {
+      screenSection = ''
+        Option         "metamodes" "DP-2: 3440x1440_120 +1920+0 {ForceCompositionPipeline=On, ForceFullCompositionPipeline=On, AllowGSYNCCompatible=On}, DP-0: 1920x1200_60 +0+240 {ForceCompositionPipeline=On, ForceFullCompositionPipeline=On}"
+        Option         "AllowIndirectGLXProtocol" "off"
+        Option         "TripleBuffer" "on"
+      '';
+
+    };
+
   };
 }
