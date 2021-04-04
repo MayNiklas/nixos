3f303b6488134c40c4cce0b18d354a1f4a6b0e81
Niklas <44636701+MayNiklas@users.noreply.github.com>
2021-04-04 20:34:02 +0200

    Add module librespeedtest

---
+++ modules/librespeedtest/default.nix
@@ -0,0 +1,33 @@
+{ lib, pkgs, config, ... }:
+with lib;
+let cfg = config.mayniklas.librespeedtest;
+in {
+
+  options.mayniklas.librespeedtest = {
+    enable = mkEnableOption "activate librespeedtest";
+    port = mkOption {
+      type = types.str;
+      default = "8080";
+    };
+    title = mkOption {
+      type = types.str;
+      default = "LibreSpeed";
+    };
+  };
+
+  config = mkIf cfg.enable {
+
+    virtualisation.oci-containers.containers.librespeedtest = {
+      autoStart = true;
+      image = "adolfintel/speedtest";
+      environment = {
+        TITLE = "${cfg.title}";
+        ENABLE_ID_OBFUSCATION = "true";
+        WEBPORT = "${cfg.port}";
+        MODE = "standalone";
+      };
+      ports = [ "${cfg.port}:${cfg.port}/tcp" ];
+    };
+
+  };
+}
