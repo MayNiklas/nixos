38373b15be27b17558e3b81c9e4594b1ee84db01
MayNiklas <info@niklas-steffen.de>
2021-04-04 13:29:34 +0200

    Add options to module pihole

--- modules/pihole/default.nix
+++ modules/pihole/default.nix
@@ -5,6 +5,18 @@ in {
 
   options.mayniklas.pihole = {
     enable = mkEnableOption "activate pihole" // { default = true; };
+    port = mkOption {
+      type = types.str;
+      default = "8080";
+    };
+    timezone = mkOption {
+      type = types.str;
+      default = "Europe/Berlin";
+    };
+    path = mkOption {
+      type = types.str;
+      default = "/docker/pihole";
+    };
   };
 
   config = mkIf cfg.enable {
@@ -12,12 +24,17 @@ in {
     virtualisation.oci-containers.containers.pihole = {
       autoStart = true;
       image = "pihole/pihole:latest";
-      environment = { TZ = "Europe/Berlin"; };
-      ports =
-        [ "53:53/tcp" "53:53/udp" "67:67/udp" "8080:80/tcp" "443:443/tcp" ];
+      environment = { TZ = "${cfg.timezone}"; };
+      ports = [
+        "53:53/tcp"
+        "53:53/udp"
+        "67:67/udp"
+        "${cfg.port}:80/tcp"
+        "443:443/tcp"
+      ];
       volumes = [
-        "/docker/pihole/etc-pihole:/etc/pihole:rw"
-        "/docker/pihole/etc-dnsmasq.d:/etc/dnsmasq.d:rw"
+        "${cfg.path}/etc-pihole:/etc/pihole:rw"
+        "${cfg.path}/etc-dnsmasq.d:/etc/dnsmasq.d:rw"
       ];
     };
 
