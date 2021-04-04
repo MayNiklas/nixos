3864f2c0e42507189fea2495e479cd466c7f721b
MayNiklas <info@niklas-steffen.de>
2021-04-04 23:23:16 +0200

    use hardware clock time

--- modules/locale/default.nix
+++ modules/locale/default.nix
@@ -10,7 +10,10 @@ in {
   config = mkIf cfg.enable {
 
     # Set your time zone.
-    time.timeZone = "Europe/Berlin";
+    time = {
+      timeZone = "Europe/Berlin";
+      hardwareClockInLocalTime = true;
+    };
 
     # Select internationalisation properties.
     i18n.defaultLocale = "en_US.UTF-8";
