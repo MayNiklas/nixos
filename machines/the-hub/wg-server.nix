a9cd27853b850663984e1db105a678830fc965e8
Niklas <44636701+MayNiklas@users.noreply.github.com>
2021-04-14 00:02:21 +0200

    Add wireguard clients

--- machines/the-hub/wg-server.nix
+++ machines/the-hub/wg-server.nix
@@ -37,24 +37,52 @@
 
       peers = [
         {
-          publicKey = "l8IA2CW95sT+fURHmUkxmfpb78amqodNHUs5tk71xRM=";
-          allowedIPs = [ "10.88.88.2/32" ];
+          publicKey = "dR4HocvCeIAq/+CmjfQxZ5lnPC9revbMSU1J7gB9Cl0=";
+          allowedIPs = [ "10.88.88.2/32" "192.168.5.0/24" "192.168.15.0/24" "192.168.20.0/24" ];
         }
+        {
+          publicKey = "NeHl2LXjivsfYlBjfTz6fuMV/Wf/95j3Sb69WyKLrnI=";
+          allowedIPs = [ "10.88.88.3/32" "192.168.88.0/24" "192.168.99.0/24" "192.168.23.8/29" ];
+        }
+        {
+          publicKey = "m8X/POOpYspeCITzRKLWPQBl2jdKYine0XCAC8lVYCE=";
+          allowedIPs = [ "10.88.88.4/32" "192.168.42.0/24" ];
+        }
+        {
+          publicKey = "ffd7LTN66FX269qTyDxT2e4kfuo7b5cRQruUzneVOzs=";
+          allowedIPs = [ "10.88.88.5/32" "192.168.98.0/24" ];
+        }
+        {
+          publicKey = "x6r6/fKy1Yq2jqf0MngnpEJ6dPa/elf3nYPmNJ+5Myc=";
+          allowedIPs = [ "10.88.88.6/32" "192.168.22.0/24" ];
+        }
+        {
+          publicKey = "fnuGuNVPco092ZV9zDNYY55tW/pzdRBltij9X/5DcUU=";
+          allowedIPs = [ "10.88.88.9/32" ];
+        }
+        # 13 inch MacBook Pro
         {
           publicKey = "DiYQ6+0EvA6kG5w07I+y5GUp1Qxekj3txjgLmK9q3CI=";
           allowedIPs = [ "10.88.88.20/32" ];
         }
+        # 16 inch MacBook Pro 
         {
-          publicKey = "2x2O/fxtOjTJgKRxd3S/Z0jUFGw2S3etdqYTiBdHVwM=";
+          publicKey = "l8IA2CW95sT+fURHmUkxmfpb78amqodNHUs5tk71xRM=";
           allowedIPs = [ "10.88.88.21/32" ];
         }
+        # 12.9 inch iPad Pro
         {
           publicKey = "pt5IcSnJdIKl12sNEZuV2vnR3qVQOuXASvd1OfWuZAU=";
           allowedIPs = [ "10.88.88.22/32" ];
         }
+        # iPhone
         {
           publicKey = "IG6shrAvupzelwaFBYDd476Xu9uUehoMYAcz80mGTGE=";
           allowedIPs = [ "10.88.88.23/32" ];
+        }
+        {
+          publicKey = "tY4kwJFserZZTHsfBzaQX05zFbPoPf90EYFQOmaE+zY=";
+          allowedIPs = [ "10.88.88.150/32" ];
         }
       ];
     };
