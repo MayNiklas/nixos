9ce865a3714d43c0a0b83fec0cbbc3cdc2d88036
MayNiklas <info@niklas-steffen.de>
2021-04-14 14:58:17 +0200

    add ipv6 to the-hub

--- machines/the-hub/configuration.nix
+++ machines/the-hub/configuration.nix
@@ -1,9 +1,14 @@
 { self, ... }:
 
-{
+let
+  interface = "ens3";
+  subnet = "64";
+  network = "2a03:4000:3f:5d::/${subnet}";
+  own_ip = "2a03:4000:3f:5d:98d5:41ff:feca:d0e7/${subnet}";
+in {
 
   imports = [ ./wg-server.nix ];
-  
+
   mayniklas = {
     server = {
       enable = true;
@@ -12,7 +17,18 @@
     kvm-guest.enable = true;
   };
 
-  networking = { hostName = "the-hub"; };
+  networking = {
+    hostName = "the-hub";
+    enableIPv6 = true;
+    useDHCP = true;
+    dhcpcd.persistent = true;
+    dhcpcd.extraConfig = ''
+      noipv6rs
+      interface ${interface}
+      ia_pd 1/${network} ${interface}
+      static ip6_address=${own_ip}
+    '';
+  };
 
   system.stateVersion = "20.09";
 
