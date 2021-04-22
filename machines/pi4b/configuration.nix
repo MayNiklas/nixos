5ef8352775741f9f9e1bcd9fb44034e4cac10eb3
MayNiklas <info@niklas-steffen.de>
2021-04-22 15:44:14 +0200

    pi4b uses mayniklas.pi4b module

--- machines/pi4b/configuration.nix
+++ machines/pi4b/configuration.nix
@@ -1,29 +1,14 @@
 { self, ... }: {
 
-  imports = [ ./hardware-configuration.nix ];
-
   mayniklas = {
     octoprint.enable = true;
+    pi4b.enable = true;
     server = {
       enable = true;
       homeConfig = { imports = [ ../../home-manager/home-server.nix ]; };
     };
     system = "aarch64-linux";
     system-config = "aarch64-unknown-linux-gnu";
-  };
-
-  # Required for the Wireless firmware
-  hardware.enableRedistributableFirmware = true;
-
-  boot.loader = {
-    raspberryPi = {
-      enable = true;
-      version = 4;
-    };
-    # Use the extlinux boot loader. (NixOS wants to enable GRUB by default)
-    grub.enable = false;
-    # Enables the generation of /boot/extlinux/extlinux.conf
-    generic-extlinux-compatible.enable = true;
   };
 
   networking = {
