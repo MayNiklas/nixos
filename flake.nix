f1ccbb39c5824d29726d8ddb06c9b4a384d3612c
MayNiklas <info@niklas-steffen.de>
2021-06-03 17:36:08 +0200

    Use my own nixpkgs

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A very basic flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:MayNiklas/nixpkgs/nixos-unstable";
 
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
