04aedd58c5efda9d4e963520d2861f81db45314f
Niklas <44636701+MayNiklas@users.noreply.github.com>
2021-06-11 10:46:27 +0200

    cache.lounge.rocks priority 10

--- modules/nix-common/default.nix
+++ modules/nix-common/default.nix
@@ -30,7 +30,7 @@ in {
       binaryCachePublicKeys =
         [ "cache.lounge.rocks:uXa8UuAEQoKFtU8Om/hq6d7U+HgcrduTVr8Cfl6JuaY=" ];
       binaryCaches =
-        [ "https://cache.nixos.org" "https://cache.lounge.rocks?priority=50" ];
+        [ "https://cache.nixos.org" "https://cache.lounge.rocks?priority=10" ];
       trustedBinaryCaches =
         [ "https://cache.lounge.rocks" "https://cache.nixos.org" ];
 
