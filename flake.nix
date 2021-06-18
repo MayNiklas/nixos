30fbf3e24be40ffa1cb7e7ad714fff78b35b991a
Niklas <44636701+MayNiklas@users.noreply.github.com>
2021-06-18 14:34:01 +0200

    Use home-manager 21.05

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
 
     flake-utils.url = "github:numtide/flake-utils";
 
-    home-manager.url = "github:nix-community/home-manager";
+    home-manager.url = "github:nix-community/home-manager/release-21.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
   };
