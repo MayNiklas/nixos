ee71fd5233d8bc72dba1d49592b705585d6268d8
Niklas <44636701+MayNiklas@users.noreply.github.com>
2021-07-04 16:49:29 +0200

    remove unstable for unfree packages

--- overlays/default.nix
+++ overlays/default.nix
@@ -11,18 +11,9 @@ self: super: {
 
   # override with newer version from nixpkgs-unstable (home-manager related)
   chromium = self.unstable.chromium;
-  discord = self.unstable.discord;
   firefox = self.unstable.firefox;
   neovim-unwrapped = self.unstable.neovim-unwrapped;
   obs-studio = self.unstable.obs-studio;
   signal-desktop = self.unstable.signal-desktop;
-  spotify = self.unstable.spotify;
-  sublime-merge = self.unstable.sublime-merge;
-  sublime3 = self.unstable.sublime3;
-  teamspeak_client = self.unstable.teamspeak_client;
-  tdesktop = self.unstable.tdesktop;
-  thunderbird-bin = self.unstable.thunderbird-bin;
-  vscode = self.unstable.vscode;
   youtube-dl = self.unstable.youtube-dl;
-  zoom-us = self.unstable.zoom-us;
 }
