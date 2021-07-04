d1ae80a331dd1aaf26146205c6f993a628d0b092
MayNiklas <info@niklas-steffen.de>
2021-07-04 15:33:35 +0200

    install vim plugins

--- home-manager/modules/vim.nix
+++ home-manager/modules/vim.nix
@@ -5,6 +5,14 @@
     viAlias = true;
     vimAlias = true;
     withPython3 = true;
+
+    plugins = with pkgs.vimPlugins; [
+      ansible-vim
+      i3config-vim
+      vim-better-whitespace
+      vim-nix
+    ];
+    
   };
 
 }
