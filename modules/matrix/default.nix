99fc78dafea18bc9b5511acaf518ad32da56326d
MayNiklas <info@niklas-steffen.de>
2021-07-11 13:03:17 +0200

    add postgresql db to matrix module

--- modules/matrix/default.nix
+++ modules/matrix/default.nix
@@ -16,6 +16,15 @@ in {
 
   config = mkIf cfg.enable {
 
+    services.postgresql.enable = true;
+    services.postgresql.initialScript = pkgs.writeText "synapse-init.sql" ''
+      CREATE ROLE "matrix-synapse" WITH LOGIN PASSWORD 'synapse';
+      CREATE DATABASE "matrix-synapse" WITH OWNER "matrix-synapse"
+        TEMPLATE template0
+        LC_COLLATE = "C"
+        LC_CTYPE = "C";
+    '';
+
     services.matrix-synapse = {
       enable = true;
       server_name = "${cfg.hostname}";
