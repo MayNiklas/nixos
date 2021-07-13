ef3e72608b42c25d2a579c137f63b9eb6cd9af16
MayNiklas <info@niklas-steffen.de>
2021-07-13 15:47:30 +0200

    add host enoch

---
+++ machines/enoch/configuration.nix
@@ -0,0 +1,45 @@
+{ self, ... }: {
+
+  imports = [
+    ../../users/marek.nix
+    ../../users/nik.nix
+    ../../users/root.nix
+  ];
+
+  mayniklas = {
+    fonts.enable = true;
+    locale.enable = true;
+    kde.enable = true;
+    nix-common.enable = true;
+    openssh.enable = true;
+    zsh.enable = true;
+    vmware-guest.enable = true;
+  };
+
+  environment.systemPackages =
+    with self.inputs.nixpkgs.legacyPackages.x86_64-linux; [
+      bash-completion
+      git
+      nixfmt
+      wget
+    ];
+
+  home-manager.users.marek = {
+    imports = [
+      ./marek.nix
+      { nixpkgs.overlays = [ self.overlay self.overlay-unstable ]; }
+    ];
+  };
+
+  home-manager.users.nik = {
+    imports = [
+      ../../home-manager/home-server.nix
+      { nixpkgs.overlays = [ self.overlay self.overlay-unstable ]; }
+    ];
+  };
+
+  networking.hostName = "enoch";
+
+  system.stateVersion = "20.09";
+
+}
---
+++ users/marek.nix
@@ -0,0 +1,16 @@
+{ config, pkgs, lib, ... }: {
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.marek = {
+    isNormalUser = true;
+    home = "/home/marek";
+    shell = pkgs.zsh;
+    openssh.authorizedKeys.keyFiles = [
+      (builtins.fetchurl {
+        url = "https://github.com/User12323.keys";
+        sha256 = "1mpra26kk2f9f3xwnqhmgb627wngsz9i0d95niyfriikxw6g5wwj";
+      })
+    ];
+  };
+
+  nix.allowedUsers = [ "marek" ];
+}
