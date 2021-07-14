4637daa9f75ee9bbb854443543c0a71dc706079f
Niklas <44636701+MayNiklas@users.noreply.github.com>
2021-07-14 19:39:49 +0200

    add user Julian

---
+++ users/julian.nix
@@ -0,0 +1,16 @@
+{ config, pkgs, lib, ... }: {
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.julian = {
+    isNormalUser = true;
+    home = "/home/julian";
+    shell = pkgs.zsh;
+    openssh.authorizedKeys.keyFiles = [
+      (builtins.fetchurl {
+        url = "https://github.com/building-from-source.keys";
+        sha256 = "0z4jhi81ql6kq37gjfdkn192ilpp282ypri1ilhak9696hhs77p9";
+      })
+    ];
+  };
+
+  nix.allowedUsers = [ "julian" ];
+}
