bfe33d46af378407459bf37defa11e6753144d06
MayNiklas <info@niklas-steffen.de>
2021-07-27 21:19:33 +0200

    nvidia: add mayniklas.nvidia.beta-driver option

--- modules/nvidia/default.nix
+++ modules/nvidia/default.nix
@@ -3,10 +3,15 @@ with lib;
 let cfg = config.mayniklas.nvidia;
 in {
 
-  options.mayniklas.nvidia = { enable = mkEnableOption "activate nvidia"; };
+  options.mayniklas.nvidia = {
+    enable = mkEnableOption "activate nvidia";
+    beta-driver = mkEnableOption "use nvidia-beta driver";
+  };
 
   config = mkIf cfg.enable {
     services.xserver = { videoDrivers = [ "nvidia" ]; };
+    hardware.nvidia.package =
+      mkIf cfg.beta-driver config.boot.kernelPackages.nvidiaPackages.beta;
     nixpkgs.config.allowUnfreePredicate = pkg:
       builtins.elem (lib.getName pkg) [ "nvidia-x11" "nvidia-settings" ];
   };
