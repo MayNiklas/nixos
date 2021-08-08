96764f30eb2f1b71b67f7797f9c8cc0df0688444
MayNiklas <info@niklas-steffen.de>
2021-08-08 17:09:49 +0200

    add dbus-launch to .xinitrc

--- modules/xserver/default.nix
+++ modules/xserver/default.nix
@@ -34,6 +34,7 @@ in {
         session = [{
           name = "home-manager";
           start = ''
+            export `dbus-launch`
             ${pkgs.runtimeShell} $HOME/.hm-xsession &
              waitPID=$!
           '';
