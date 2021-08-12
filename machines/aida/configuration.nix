5efae828c976caf73153a2fe6aea74500731e2c8
Niklas <44636701+MayNiklas@users.noreply.github.com>
2021-08-12 19:01:45 +0200

    mount snowflake on aida

--- machines/aida/configuration.nix
+++ machines/aida/configuration.nix
@@ -21,6 +21,13 @@
       [ "nolock" "soft" "ro" "nounix" "dir_mode=0777" "file_mode=0777" ];
     fsType = "cifs";
   };
+  
+  fileSystems."/mnt/snowflake" = {
+    device = "//snowflake/public";
+    options =
+      [ "nolock" "soft" "ro" "nounix" "dir_mode=0777" "file_mode=0777" ];
+    fsType = "cifs";
+  };
 
   networking.hostName = "aida";
 
