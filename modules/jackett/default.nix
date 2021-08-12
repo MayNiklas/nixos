a467746e79a0e5b27473c3f66b7cc3fb1419e1e4
MayNiklas <info@niklas-steffen.de>
2021-08-12 17:49:17 +0200

    add module jackett

---
+++ modules/jackett/default.nix
@@ -0,0 +1,17 @@
+{ lib, pkgs, config, ... }:
+with lib;
+let cfg = config.mayniklas.jackett;
+in {
+
+  options.mayniklas.jackett = { enable = mkEnableOption "activate jackett"; };
+
+  config = mkIf cfg.enable {
+    services.jackett = {
+      enable = true;
+      user = "jackett";
+      group = "jackett";
+      dataDir = "/var/lib/jackett/.config/Jackett";
+    };
+    networking.firewall = { allowedTCPPorts = [ 9117 ]; };
+  };
+}
