6a0396be898e8ad23c8a5061f977ae3dfc0e7ce6
MayNiklas <info@niklas-steffen.de>
2021-08-16 17:55:33 +0200

    add domain to grafana

--- modules/monitoring/grafana.nix
+++ modules/monitoring/grafana.nix
@@ -6,12 +6,20 @@ in {
   options.mayniklas.services.monitoring-server.dashboard = {
     enable = mkEnableOption "Grafana dashboard";
     openFirewall = mkEnableOption "Open firewall for Grafana";
+
+    domain = mkOption {
+      type = types.str;
+      default = "status.nik-ste.de";
+      example = "dashboards.myhost.com";
+      description = "Domain for grafana";
+    };
   };
 
   config = mkIf cfg.enable {
     # Graphana fronend
     services.grafana = {
       enable = true;
+      domain = cfg.domain;
       # Default is 3000
       port = 9005;
       addr = "127.0.0.1";
