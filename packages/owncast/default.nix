0a3430812cc14777b1c960a50a0ae2505ad2ad28
Niklas <44636701+MayNiklas@users.noreply.github.com>
2021-08-26 18:33:55 +0200

    build owncast from source

--- packages/owncast/default.nix
+++ packages/owncast/default.nix
@@ -1,31 +1,27 @@
-{ lib, stdenv, fetchurl, unzip, autoPatchelfHook, ... }:
+{ lib, buildGoModule, fetchFromGitHub, ffmpeg }:
 
-stdenv.mkDerivation rec {
+buildGoModule rec {
+
   pname = "owncast";
-  version = "0.0.8";
-
-  src = fetchurl {
-    url =
-      "https://github.com/owncast/${pname}/releases/download/v${version}/${pname}-${version}-linux-64bit.zip";
-    sha256 = "sha256-5A/qYmJL12suoUq6sQrOHxmmcXlfKwsWEkTs0fb0wUk=";
+  version = "0.0.6";
+  
+  src = fetchFromGitHub {
+    owner = "owncast";
+    repo = "owncast";
+    rev = "v${version}";
+    sha256 = "0vkh4lb5qp20y476j3w0wrshg0bp8kn496bbhdygw8yv3yra45c6";
   };
-  nativeBuildInputs = [
-    autoPatchelfHook
-    unzip
-  ];
-  sourceRoot = ".";
-
-  installPhase = ''
-    mkdir -p $out/bin
-    cp owncast "$out"/bin/owncast
-  '';
-
+  
+  vendorSha256 = "0m4xqqpxprbgipwg9ixbjl73zbmlwynmzb1hbgrcgnv02g9mzicf";
+  
+  propagatedBuildInputs = [ ffmpeg ];
+  
   meta = with lib; {
-    description =
-      "Owncast is a self-hosted live video and web chat server for use with existing popular broadcasting software.";
-    homepage = "https://owncast.online/";
+    description = "self-hosted video live streaming solution";
+    homepage = "https://owncast.online";
     license = licenses.mit;
-    platforms = [ "x86_64-linux" "aarch64-linux" ];
-    maintainers = with maintainers; [ mayniklas ];
+    platforms = platforms.unix;
   };
+  
 }
+
