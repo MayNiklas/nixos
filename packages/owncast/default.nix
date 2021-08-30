d2fa9146d841a19acea4eca21e1096678b698671
Niklas <44636701+MayNiklas@users.noreply.github.com>
2021-08-30 17:44:49 +0200

    owncast: wrap dependencies PATH

--- packages/owncast/default.nix
+++ packages/owncast/default.nix
@@ -1,26 +1,35 @@
-{ lib, buildGoModule, fetchFromGitHub, ffmpeg }:
+{ lib, buildGoModule, fetchFromGitHub, ffmpeg, makeWrapper, which, ... }:
 
 buildGoModule rec {
 
   pname = "owncast";
   version = "0.0.8";
-  
+
   src = fetchFromGitHub {
     owner = "owncast";
     repo = "owncast";
     rev = "v${version}";
     sha256 = "0md4iafa767yxkwh6z8zpcjv9zd79ql2wapx9vzyd973ksvrdaw2";
   };
-  
+
   vendorSha256 = "sha256-bH2CWIgpOS974/P98n0R9ebGTJ0YoqPlH8UmxSYNHeM=";
-  
+
   propagatedBuildInputs = [ ffmpeg ];
-  
+
+  buildInputs = [ makeWrapper ];
+
+  postInstall = ''
+    wrapProgram $out/bin/owncast --prefix PATH : ${
+      lib.makeBinPath [ which ffmpeg ]
+    }
+  '';
+
   meta = with lib; {
     description = "self-hosted video live streaming solution";
     homepage = "https://owncast.online";
     license = licenses.mit;
     platforms = platforms.unix;
+    maintainers = with maintainers; [ mayniklas ];
   };
-  
+
 }
