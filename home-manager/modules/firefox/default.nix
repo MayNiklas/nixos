d228fdf429143a6dd1f079c8215574cbfc9447bd
MayNiklas <info@niklas-steffen.de>
2021-09-25 15:24:20 +0200

    add options to firefox module

--- home-manager/modules/firefox/default.nix
+++ home-manager/modules/firefox/default.nix
@@ -1,13 +1,20 @@
-{ config, pkgs, lib, ... }: {
-  programs.firefox = {
-    enable = true;
-    package = pkgs.firefox-bin;
-    profiles = {
-      nik = {
-        isDefault = true;
-        settings = {
-          "browser.startup.homepage" = "https://github.com/MayNiklas";
-          "devtools.theme" = "dark";
+{ lib, pkgs, config, ... }:
+with lib;
+let cfg = config.mayniklas.programs.devolopment;
+in {
+  options.mayniklas.programs.firefox.enable = mkEnableOption "enable firefox";
+
+  config = mkIf cfg.enable {
+    programs.firefox = {
+      enable = true;
+      package = pkgs.firefox-bin;
+      profiles = {
+        nik = {
+          isDefault = true;
+          settings = {
+            "browser.startup.homepage" = "https://github.com/MayNiklas";
+            "devtools.theme" = "dark";
+          };
         };
       };
     };
