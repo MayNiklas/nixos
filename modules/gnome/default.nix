8030163476d08d1ff1e3b282069e298cb43a6f28
Niklas <44636701+MayNiklas@users.noreply.github.com>
2021-10-10 17:05:34 +0200

    add gnome module

---
+++ modules/gnome/default.nix
@@ -0,0 +1,20 @@
+{ lib, pkgs, config, ... }:
+with lib;
+let cfg = config.mayniklas.gnome;
+in {
+
+  options.mayniklas.gnome = { enable = mkEnableOption "activate gnome"; };
+
+  config = mkIf cfg.enable {
+
+    services.xserver = {
+      enable = true;
+      displayManager.gdm.enable = true;
+      desktopManager.gnome.enable = true;
+    };
+
+    environment.systemPackages = with pkgs; [ gnomeExtensions.appindicator ];
+    services.udev.packages = with pkgs; [ gnome3.gnome-settings-daemon ];
+
+  };
+}
