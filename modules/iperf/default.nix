1831eb0528e1b16c282ff1b88dad731427cf8412
MayNiklas <info@niklas-steffen.de>
2021-12-14 16:20:27 +0100

    add iperf.openFirewall

--- modules/iperf/default.nix
+++ modules/iperf/default.nix
@@ -3,7 +3,19 @@ with lib;
 let cfg = config.mayniklas.iperf;
 in {
 
-  options.mayniklas.iperf = { enable = mkEnableOption "activate iperf"; };
+  options.mayniklas.iperf = {
+
+    enable = mkEnableOption "activate iperf";
+
+    openFirewall = mkOption {
+      type = types.bool;
+      default = false;
+      description = ''
+        Open the appropriate ports in the firewall for owncast.
+      '';
+    };
+
+  };
 
   config = mkIf cfg.enable {
 
@@ -12,8 +24,11 @@ in {
       openFirewall = false;
       port = 5201;
     };
-    
-    networking.firewall = { interfaces.wg0.allowedTCPPorts = [ 5201 ]; };
+
+    networking.firewall = {
+      allowedTCPPorts = mkIf cfg.openFirewall [ 5201 ];
+      interfaces = { wg0.allowedTCPPorts = [ 5201 ]; };
+    };
 
   };
 }
