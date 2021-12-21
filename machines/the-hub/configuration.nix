f731b8f36847273d59851f587e0a751e63c6c908
MayNiklas <info@niklas-steffen.de>
2021-12-21 19:42:01 +0100

    hardcode the-hub wg0

--- machines/the-hub/configuration.nix
+++ machines/the-hub/configuration.nix
@@ -20,10 +20,6 @@ in {
       node.enable = true;
       blackbox.enable = true;
     };
-    wg = {
-      enable = true;
-      server = true;
-    };
     services.monitoring-server = {
       enable = true;
       loki = { enable = true; };
@@ -97,6 +93,7 @@ in {
       # only to renew certificate!
       # allowedTCPPorts = [ 80 443 ];
       interfaces.wg0.allowedTCPPorts = [ 80 443 ];
+      allowedUDPPorts = [ 58102 ];
     };
 
     wireguard.interfaces.wg0 = {
@@ -113,89 +110,96 @@ in {
       '';
     };
 
-    wireguard.interfaces.wg0.peers = [
-      # S2S home
-      {
-        publicKey = "lgEYddHkOxjEVqgnQruhwsRa/riiGm1jgcInDtCjfiU=";
-        allowedIPs = [
-          "10.88.88.2/32"
-          "192.168.5.0/24"
-          "192.168.15.0/24"
-          "192.168.20.0/24"
-          "192.168.30.0/24"
-        ];
-      }
-      # S2S G.
-      {
-        publicKey = "NeHl2LXjivsfYlBjfTz6fuMV/Wf/95j3Sb69WyKLrnI=";
-        allowedIPs = [
-          "10.88.88.3/32"
-          "192.168.88.0/24"
-          "192.168.99.0/24"
-          "192.168.23.8/29"
-        ];
-      }
-      # S2S M.
-      {
-        publicKey = "UxM4yRr6Vd5wYPtGcMcYd7pfyKZCGyz9VsG31FVbo38=";
-        allowedIPs = [ "10.88.88.4/32" "192.168.42.0/24" "192.168.0.1/32" ];
-      }
-      # S2S Lep.
-      {
-        publicKey = "qGTvKWwkrdbaUvnTmkwvypWwmslKYitZ3h50F6JLFFM=";
-        allowedIPs = [ "10.88.88.5/32" "192.168.98.0/24" ];
-      }
-      # S2S K.
-      {
-        publicKey = "x6r6/fKy1Yq2jqf0MngnpEJ6dPa/elf3nYPmNJ+5Myc=";
-        allowedIPs = [ "10.88.88.6/32" "192.168.22.0/24" ];
-      }
-      # S2S A.
-      {
-        publicKey = "uYDIYQ1ZedQ4DZSESlAZogf3jynusk7g+UnI2ZfU01k=";
-        allowedIPs = [ "10.88.88.7/32" "192.168.97.0/24" ];
-      }
-      # iMac B.
-      {
-        publicKey = "fnuGuNVPco092ZV9zDNYY55tW/pzdRBltij9X/5DcUU=";
-        allowedIPs = [ "10.88.88.9/32" ];
-      }
-      # 13 inch MacBook Pro
-      {
-        publicKey = "DiYQ6+0EvA6kG5w07I+y5GUp1Qxekj3txjgLmK9q3CI=";
-        allowedIPs = [ "10.88.88.20/32" ];
-      }
-      # 16 inch MacBook Pro
-      {
-        publicKey = "l8IA2CW95sT+fURHmUkxmfpb78amqodNHUs5tk71xRM=";
-        allowedIPs = [ "10.88.88.21/32" ];
-      }
-      # 12.9 inch iPad Pro
-      {
-        publicKey = "pt5IcSnJdIKl12sNEZuV2vnR3qVQOuXASvd1OfWuZAU=";
-        allowedIPs = [ "10.88.88.22/32" ];
-      }
-      # iPhone
-      {
-        publicKey = "IG6shrAvupzelwaFBYDd476Xu9uUehoMYAcz80mGTGE=";
-        allowedIPs = [ "10.88.88.23/32" ];
-      }
-      # water-on-fire
-      {
-        publicKey = "dWMWrCKd/vTDhs+15YiFeSQziZACBiOK/f3vC5x73Qc=";
-        allowedIPs = [ "10.88.88.24/32" ];
-      }
-      # MacBook M.
-      {
-        publicKey = "ylprZ5gCugAxo1lkdbSWWDicVHd+Ul+2rz9ItuMJxxs=";
-        allowedIPs = [ "10.10.10.7/32" ];
-      }
-      # MacBook A.
-      {
-        publicKey = "+UaFc1DRQIRgCU38G9qi7k2BiCUbIZVMYdYT/ZUyuCU=";
-        allowedIPs = [ "10.88.88.200/32" ];
-      }
-    ];
+    wireguard.interfaces.wg0 = {
+      ips = [ "10.88.88.1/24" ];
+      listenPort = 58102;
+      # Path to the private key file
+      privateKeyFile = toString /var/src/secrets/wireguard/private;
+      generatePrivateKeyFile = true;
+      peers = [
+        # S2S home
+        {
+          publicKey = "lgEYddHkOxjEVqgnQruhwsRa/riiGm1jgcInDtCjfiU=";
+          allowedIPs = [
+            "10.88.88.2/32"
+            "192.168.5.0/24"
+            "192.168.15.0/24"
+            "192.168.20.0/24"
+            "192.168.30.0/24"
+          ];
+        }
+        # S2S G.
+        {
+          publicKey = "NeHl2LXjivsfYlBjfTz6fuMV/Wf/95j3Sb69WyKLrnI=";
+          allowedIPs = [
+            "10.88.88.3/32"
+            "192.168.88.0/24"
+            "192.168.99.0/24"
+            "192.168.23.8/29"
+          ];
+        }
+        # S2S M.
+        {
+          publicKey = "UxM4yRr6Vd5wYPtGcMcYd7pfyKZCGyz9VsG31FVbo38=";
+          allowedIPs = [ "10.88.88.4/32" "192.168.42.0/24" "192.168.0.1/32" ];
+        }
+        # S2S Lep.
+        {
+          publicKey = "qGTvKWwkrdbaUvnTmkwvypWwmslKYitZ3h50F6JLFFM=";
+          allowedIPs = [ "10.88.88.5/32" "192.168.98.0/24" ];
+        }
+        # S2S K.
+        {
+          publicKey = "x6r6/fKy1Yq2jqf0MngnpEJ6dPa/elf3nYPmNJ+5Myc=";
+          allowedIPs = [ "10.88.88.6/32" "192.168.22.0/24" ];
+        }
+        # S2S A.
+        {
+          publicKey = "uYDIYQ1ZedQ4DZSESlAZogf3jynusk7g+UnI2ZfU01k=";
+          allowedIPs = [ "10.88.88.7/32" "192.168.97.0/24" ];
+        }
+        # iMac B.
+        {
+          publicKey = "fnuGuNVPco092ZV9zDNYY55tW/pzdRBltij9X/5DcUU=";
+          allowedIPs = [ "10.88.88.9/32" ];
+        }
+        # 13 inch MacBook Pro
+        {
+          publicKey = "DiYQ6+0EvA6kG5w07I+y5GUp1Qxekj3txjgLmK9q3CI=";
+          allowedIPs = [ "10.88.88.20/32" ];
+        }
+        # 16 inch MacBook Pro
+        {
+          publicKey = "l8IA2CW95sT+fURHmUkxmfpb78amqodNHUs5tk71xRM=";
+          allowedIPs = [ "10.88.88.21/32" ];
+        }
+        # 12.9 inch iPad Pro
+        {
+          publicKey = "pt5IcSnJdIKl12sNEZuV2vnR3qVQOuXASvd1OfWuZAU=";
+          allowedIPs = [ "10.88.88.22/32" ];
+        }
+        # iPhone
+        {
+          publicKey = "IG6shrAvupzelwaFBYDd476Xu9uUehoMYAcz80mGTGE=";
+          allowedIPs = [ "10.88.88.23/32" ];
+        }
+        # water-on-fire
+        {
+          publicKey = "dWMWrCKd/vTDhs+15YiFeSQziZACBiOK/f3vC5x73Qc=";
+          allowedIPs = [ "10.88.88.24/32" ];
+        }
+        # MacBook M.
+        {
+          publicKey = "ylprZ5gCugAxo1lkdbSWWDicVHd+Ul+2rz9ItuMJxxs=";
+          allowedIPs = [ "10.10.10.7/32" ];
+        }
+        # MacBook A.
+        {
+          publicKey = "+UaFc1DRQIRgCU38G9qi7k2BiCUbIZVMYdYT/ZUyuCU=";
+          allowedIPs = [ "10.88.88.200/32" ];
+        }
+      ];
+    };
   };
 
   system.stateVersion = "20.09";
