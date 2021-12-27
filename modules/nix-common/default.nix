74f7aba3969e90424c536840fbf4ad094607e564
Niklas <44636701+MayNiklas@users.noreply.github.com>
2021-12-28 00:42:09 +0100

    remove nix.extraOptions = ca-references

--- modules/nix-common/default.nix
+++ modules/nix-common/default.nix
@@ -21,7 +21,7 @@ in {
 
       package = pkgs.nixFlakes;
       extraOptions = ''
-        experimental-features = nix-command flakes ca-references
+        experimental-features = nix-command flakes
         # Free up to 1GiB whenever there is less than 100MiB left.
         min-free = ${toString (100 * 1024 * 1024)}
         max-free = ${toString (1024 * 1024 * 1024)}
