e78fb56f124f5d56924c18bd4edf4212a6c818f2
MayNiklas <info@niklas-steffen.de>
2022-01-19 17:06:56 +0100

    add wg to the-bus & enable metrics

--- machines/the-bus/configuration.nix
+++ machines/the-bus/configuration.nix
@@ -9,6 +9,16 @@
       enable = true;
       host = "matrix.lounge.rocks";
     };
+    metrics = {
+      flake.enable = true;
+      node.enable = true;
+    };
+    wg = {
+      enable = true;
+      ip = "10.88.88.19";
+      allowedIPs = [ "10.88.88.1/32" ];
+    };
+    hosts = { enable = true; };
     kvm-guest.enable = true;
   };
 
