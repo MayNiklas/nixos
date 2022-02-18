073c2d7a2a7b8070d50b0db550168b3afd80ddd7
Niklas <44636701+MayNiklas@users.noreply.github.com>
2022-02-18 16:51:18 +0100

    add ipv6 to the-bus

--- machines/the-bus/configuration.nix
+++ machines/the-bus/configuration.nix
@@ -26,6 +26,12 @@
     hostName = "the-bus";
     firewall.interfaces.wg0.allowedTCPPorts = [ 9100 ];
     wg-quick.interfaces.wg0.postUp = "/run/wrappers/bin/ping -c 5 10.88.88.1";
+    interfaces.ens3 = {
+      ipv6.addresses = [{
+        address = "2a03:4000:6:8519::1";
+        prefixLength = 64;
+      }];
+    };
   };
 
   system.stateVersion = "20.09";
