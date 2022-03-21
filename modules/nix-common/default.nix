473bcc10184807e1a3f08b77eca3985e27635b89
MayNiklas <info@niklas-steffen.de>
2022-03-21 15:40:57 +0100

    use s3 binary cache

--- modules/nix-common/default.nix
+++ modules/nix-common/default.nix
@@ -28,15 +28,19 @@ in {
       '';
 
       # binary cache -> build by DroneCI
-      binaryCachePublicKeys = mkIf (cfg.disable-cache != true)
-        [ "cache.lounge.rocks:uXa8UuAEQoKFtU8Om/hq6d7U+HgcrduTVr8Cfl6JuaY=" ];
+      binaryCachePublicKeys = mkIf (cfg.disable-cache != true) [
+        "cache.lounge.rocks:uXa8UuAEQoKFtU8Om/hq6d7U+HgcrduTVr8Cfl6JuaY="
+        "oracle-aarch64-runner-1-1:8JgwoRCw2p2vQPdGOij5YEz+vgO1QsAhkudiHFA2XEA="
+      ];
       binaryCaches = mkIf (cfg.disable-cache != true) [
         "https://cache.nixos.org"
-        "https://cache.lounge.rocks?priority=50"
+        "https://cache.lounge.rocks?priority=100"
+        "https://s3.lounge.rocks/example-nix-cache?priority=50"
       ];
       trustedBinaryCaches = mkIf (cfg.disable-cache != true) [
-        "https://cache.lounge.rocks"
         "https://cache.nixos.org"
+        "https://cache.lounge.rocks"
+        "https://s3.lounge.rocks/example-nix-cache/"
       ];
 
       # Save space by hardlinking store files
