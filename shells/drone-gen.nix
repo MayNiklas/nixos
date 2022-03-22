a0a038fd80a6a74a23e2119f77f00ad9f0a31a07
MayNiklas <info@niklas-steffen.de>
2022-03-22 11:18:45 +0100

    build dronefile via json vars

--- shells/drone-gen.nix
+++ shells/drone-gen.nix
@@ -3,7 +3,7 @@ with pkgs;
 mkShell {
   buildInputs = [ mustache-go ];
   shellHook = ''
-    echo "[ "aida", "deke", "flint", "kora", "simmons", "snowflake", "the-bus", "the-hub", "water-on-fire" ]" | mustache drone-yaml.mustache > .drone.yml
+    mustache drone.json drone-yaml.mustache > .drone.yml
     exit
   '';
 }
