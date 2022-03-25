fb2b6895c554f8d8fbc6a36728cc53f68b17d24d
MayNiklas <info@niklas-steffen.de>
2022-03-25 16:35:31 +0100

    change s3 cache url

--- modules/nix-common/default.nix
+++ modules/nix-common/default.nix
@@ -30,17 +30,16 @@ in {
       # binary cache -> build by DroneCI
       binaryCachePublicKeys = mkIf (cfg.disable-cache != true) [
         "cache.lounge.rocks:uXa8UuAEQoKFtU8Om/hq6d7U+HgcrduTVr8Cfl6JuaY="
-        "oracle-aarch64-runner-1-1:8JgwoRCw2p2vQPdGOij5YEz+vgO1QsAhkudiHFA2XEA="
       ];
       binaryCaches = mkIf (cfg.disable-cache != true) [
         "https://cache.nixos.org"
         "https://cache.lounge.rocks?priority=100"
-        "https://s3.lounge.rocks/example-nix-cache?priority=50"
+        "https://s3.lounge.rocks/nix-cache?priority=50"
       ];
       trustedBinaryCaches = mkIf (cfg.disable-cache != true) [
         "https://cache.nixos.org"
         "https://cache.lounge.rocks"
-        "https://s3.lounge.rocks/example-nix-cache/"
+        "https://s3.lounge.rocks/nix-cache/"
       ];
 
       # Save space by hardlinking store files
