137ed6f52407a8aa3936e4c0e052598bc18f2a6e
MayNiklas <info@niklas-steffen.de>
2022-05-04 10:53:13 +0200

    replace ' -> "

--- packages/s3uploader/default.nix
+++ packages/s3uploader/default.nix
@@ -16,16 +16,16 @@ stdenv.mkDerivation rec {
       # go through all result files
       # use --out-link result-*NAME* during build
       for f in result*; do
-          for path in $(nix-store -qR $f); do
-              signatures=$(nix path-info --sigs --json $path | ${pkgs.jq}/bin/jq 'try .[].signatures[]')
+        for path in $(nix-store -qR $f); do
+              signatures=$(nix path-info --sigs --json $path | ${pkgs.jq}/bin/jq "try .[].signatures[]")
           if [[ $signatures == *"cache.lounge.rocks"* ]]; then
-              echo "add $path to upload.list"
-              echo $path >> upload.list
+            echo "add $path to upload.list"
+            echo $path >> upload.list
           fi
-          done
+        done
       done
       cat upload.list | uniq > upload
-      nix copy --to 's3://nix-cache?scheme=https&region=eu-central-1&endpoint=s3.lounge.rocks' $(cat upload)
+      nix copy --to "s3://nix-cache?scheme=https&region=eu-central-1&endpoint=s3.lounge.rocks" $(cat upload)
     '';
 
   in ''
