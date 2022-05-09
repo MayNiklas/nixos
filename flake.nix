3b49953a1dd938a4a67ae959f0d868e4a8757976
MayNiklas <info@niklas-steffen.de>
2022-05-09 16:29:34 +0200

    nixos unstable

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A very basic flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable-small";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
     flake-utils.url = "github:numtide/flake-utils";
 
