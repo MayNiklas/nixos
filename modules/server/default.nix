b048d1f66e2f810a8f5019ecfc222da3d9ad38f7
MayNiklas <info@niklas-steffen.de>
2022-05-12 18:46:08 +0200

    remove doubled home-manager

--- modules/server/default.nix
+++ modules/server/default.nix
@@ -14,78 +14,13 @@ in {
         enable home-manager for this server
       '';
     };
-    git = mkOption {
-      type = types.bool;
-      default = true;
-      description = ''
-        enable git for this server
-      '';
-    };
-    vim = mkOption {
-      type = types.bool;
-      default = true;
-      description = ''
-        enable vim for this server
-      '';
-    };
-    vs-fix = mkOption {
-      type = types.bool;
-      default = true;
-      description = ''
-        enable vs-fix for this server
-      '';
-    };
   };
 
   config = mkIf cfg.enable {
 
     services.postgresql.package = pkgs.postgresql_11;
 
-    # DON'T set useGlobalPackages! It's not necessary in newer
-    # home-manager versions and does not work with configs using
-    # nixpkgs.config`
-    home-manager.useUserPackages = true;
-
-    # nixpkgs.overlays = [ flake-self.overlays.default ];
-
-    home-manager.users.nik = mkIf cfg.home-manager {
-
-      # Let Home Manager install and manage itself.
-      programs.home-manager.enable = true;
-      programs.command-not-found.enable = true;
-      home.username = "nik";
-      home.homeDirectory = "/home/nik";
-      # Allow "unfree" licenced packages
-      nixpkgs.config = { allowUnfree = true; };
-
-      mayniklas = {
-        programs = {
-          git.enable = mkIf cfg.git true;
-          vim.enable = mkIf cfg.vim true;
-          vs-fix.enable = mkIf cfg.vs-fix true;
-          zsh.enable = true;
-        };
-      };
-
-      # Install these packages for my user
-      home.packages = with pkgs; [
-        gcc
-        htop
-        iperf3
-        nmap
-        unzip
-        drone-gen
-        vs-fix
-      ];
-
-      imports = [
-        ../../home-manager/git
-        ../../home-manager/vim
-        ../../home-manager/vs-fix
-        ../../home-manager/zsh
-      ];
-      home.stateVersion = "21.05";
-    };
+    mayniklas.user.nik.home-manager.headless = true;
 
     environment.systemPackages = with pkgs; [ bash-completion git nixfmt wget ];
 
