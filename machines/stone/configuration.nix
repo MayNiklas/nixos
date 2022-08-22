4f766b5d0b18d35902444c8a147e3ec89277f962
MayNiklas <info@niklas-steffen.de>
2022-08-22 20:54:31 +0200

    add host stone

---
+++ machines/stone/configuration.nix
@@ -0,0 +1,28 @@
+{ self, ... }: {
+
+  mayniklas = {
+    cloud.vmware-x86.enable = true;
+    hosts = { enable = true; };
+    server = {
+      enable = true;
+      home-manager = true;
+    };
+    metrics = {
+      node = {
+        enable = true;
+        flake = true;
+      };
+    };
+  };
+
+
+
+
+  networking = {
+    hostName = "stone";
+    firewall = { allowedTCPPorts = [ 9100 9115 ]; };
+  };
+
+  system.stateVersion = "20.09";
+
+}
