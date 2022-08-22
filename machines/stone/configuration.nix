47589b4934f86687d6423bf701980c5c05014174
MayNiklas <info@niklas-steffen.de>
2022-08-22 21:34:40 +0200

    add minecraft server

--- machines/stone/configuration.nix
+++ machines/stone/configuration.nix
@@ -1,4 +1,4 @@
-{ self, ... }: {
+{ pkgs, ... }: {
 
   mayniklas = {
     cloud.vmware-x86.enable = true;
@@ -14,6 +14,29 @@
       };
     };
   };
+
+  # minecraft service
+  systemd.services.minecraft-server = {
+    description = "Minecraft Server Service";
+    wantedBy = [ "multi-user.target" ];
+    after = [ "network.target" ];
+    serviceConfig = {
+      ExecStart = "${pkgs.jre_headless}/bin/java \$@ -Xms2G -Xmx2G -jar /var/lib/minecraft/paper-1.19.2-132.jar --nogui";
+      Restart = "always";
+      User = "minecraft";
+      WorkingDirectory = "/var/lib/minecraft";
+    };
+  };
+
+  # minecraft user
+  users.users.minecraft = {
+    description = "Minecraft server service user";
+    home = "/var/lib/minecraft";
+    createHome = true;
+    isSystemUser = true;
+    group = "minecraft";
+  };
+  users.groups.minecraft = { };
 
   networking = {
     hostName = "stone";
