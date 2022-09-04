c2b251a2a794c17ee423ff056a86e98ac2d53d5e
MayNiklas <info@niklas-steffen.de>
2022-09-04 16:57:35 +0200

    pve: add kernel modules

--- modules/cloud-provider/pve-x86.nix
+++ modules/cloud-provider/pve-x86.nix
@@ -27,6 +27,9 @@ in
       fsType = "vfat";
     };
 
+    boot.initrd.availableKernelModules = [ "virtio_net" "virtio_pci" "virtio_mmio" "virtio_blk" "virtio_scsi" "9p" "9pnet_virtio" ];
+    boot.initrd.kernelModules = [ "virtio_balloon" "virtio_console" "virtio_rng" ];
+
     boot.growPartition = true;
 
     boot.loader.grub = {
