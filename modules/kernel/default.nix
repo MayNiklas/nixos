67bfcc60f48a5dd1172232f17fccfb2cc4b41ac3
MayNiklas <info@niklas-steffen.de>
2022-11-25 14:05:43 +0100

    switch to default kernel

--- modules/kernel/default.nix
+++ modules/kernel/default.nix
@@ -10,10 +10,14 @@ in
 
   config = mkIf cfg.enable {
 
-    # just for fun, I want to use Kernel 6.0 on some of my machines
-    boot.kernelPackages = lib.mkIf
-      (lib.versionOlder pkgs.linux.version "6.0")
-      (lib.mkDefault pkgs.linuxPackages_6_0);
+    # # just for fun, I want to use Kernel 6.0 on some of my machines
+    # # update: I had some issues with my filesystem during nixos-rebuild,
+    # # so I disabled this for now
+    # boot.kernelPackages = lib.mkIf
+    #   (lib.versionOlder pkgs.linux.version "6.0")
+    #   (lib.mkDefault pkgs.linuxPackages_6_0);
+
+    boot.kernelPackages = pkgs.linuxPackages;
 
   };
 }
