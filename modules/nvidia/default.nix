c4d71ea6dc1ada81c74982ff1f5c483f6f92f35a
MayNiklas <info@niklas-steffen.de>
2023-02-17 10:54:06 +0100

    enable nvidia with docker

--- modules/nvidia/default.nix
+++ modules/nvidia/default.nix
@@ -10,14 +10,25 @@ in
   };
 
   config = mkIf cfg.enable {
+
     services.xserver = { videoDrivers = [ "nvidia" ]; };
+
+    # when docker is enabled, enable nvidia-docker
+    hardware.opengl.driSupport32Bit = mkIf config.virtualisation.docker.enable true;
+    virtualisation.docker.enableNvidia = mkIf config.virtualisation.docker.enable true;
+
+    # use nvidia-beta driver when beta-driver is enabled
     hardware.nvidia.package =
       mkIf cfg.beta-driver config.boot.kernelPackages.nvidiaPackages.beta;
+
     nixpkgs.config = {
+      # allow nvidia-x11 and nvidia-settings to be installed (unfree)
       allowUnfreePredicate = pkg:
         builtins.elem (lib.getName pkg) [ "nvidia-x11" "nvidia-settings" ];
+      # build packages with cudaSupport
       cudaSupport = true;
     };
+
   };
 
 }
