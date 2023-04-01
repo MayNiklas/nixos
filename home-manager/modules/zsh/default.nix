b0f46a4c0a37b9b5eff99267cf138e640ba15375
MayNiklas <info@niklas-steffen.de>
2023-04-01 11:26:20 +0200

    htop: show_cpu_temperature

--- home-manager/modules/zsh/default.nix
+++ home-manager/modules/zsh/default.nix
@@ -122,6 +122,7 @@ in
       enable = true;
       settings = {
         show_cpu_frequency = true;
+        show_cpu_temperature = true;
         show_cpu_usage = true;
         show_program_path = true;
         tree_view = false;
