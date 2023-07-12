c18757958249eb09c4b06c8cc84c7c6d25585dfa
MayNiklas <info@niklas-steffen.de>
2023-07-12 23:10:47 +0200

    use pipewire

--- modules/sound/default.nix
+++ modules/sound/default.nix
@@ -8,14 +8,25 @@ in
 
   config = mkIf cfg.enable {
 
-    # Enable sound.
+    # Enable sound with pipewire.
     sound.enable = true;
-    hardware.pulseaudio = {
+    hardware.pulseaudio.enable = false;
+    security.rtkit.enable = true;
+    services.pipewire = {
       enable = true;
-      package = pkgs.pulseaudioFull;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+      # If you want to use JACK applications, uncomment this
+      #jack.enable = true;
+
+      # use the example session manager (no others are packaged yet so this is enabled by default,
+      # no need to redefine it in your config for now)
+      # media-session.enable = true;
     };
 
-    environment.systemPackages = with pkgs; [ pavucontrol ];
+    # still needed?
+    # environment.systemPackages = with pkgs; [ pavucontrol ];
 
     users.extraUsers.${config.mayniklas.var.mainUser}.extraGroups = [ "audio" ];
 
