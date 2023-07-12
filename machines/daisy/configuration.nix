578bde7fd9cc89c19330c9d134bca062f7c91bc3
MayNiklas <info@niklas-steffen.de>
2023-07-13 00:06:34 +0200

    daisy: add nixos-hardware

--- machines/daisy/configuration.nix
+++ machines/daisy/configuration.nix
@@ -1,8 +1,10 @@
-{ config, pkgs, ... }: {
+{ config, pkgs, nixos-hardware, ... }: {
 
   imports = [
     # Include the results of the hardware scan.
     ./hardware-configuration.nix
+    # https://github.com/NixOS/nixos-hardware/tree/master/lenovo/thinkpad/x390
+    nixos-hardware.nixosModules.lenovo-thinkpad-x390
   ];
 
   mayniklas = {
