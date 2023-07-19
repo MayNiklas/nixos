cd6fef2a10483fddb7922ef5b10df4f343b0521e
MayNiklas <info@niklas-steffen.de>
2023-07-19 23:55:05 +0200

    add sway config

--- modules/sway/default.nix
+++ modules/sway/default.nix
@@ -48,13 +48,88 @@ in
             }
           ];
 
-          # TODO: Laptop specific stuff should be enabled by a function 
-          keybindings = let modifier = config.wayland.windowManager.sway.config.modifier; in
-            lib.mkOptionDefault {
-              # control brightness
-              "XF86MonBrightnessUp" = "exec ${pkgs.light}/bin/light -A 10";
-              "XF86MonBrightnessDown" = "exec ${pkgs.light}/bin/light -U 10";
-            };
+          keybindings =
+            let
+              inherit (config.home-manager.users.nik.wayland.windowManager.sway.config) modifier left down up right menu terminal;
+            in
+            {
+              "${modifier}+Return" = "exec ${terminal}";
+              "${modifier}+Shift+q" = "kill";
+              "${modifier}+d" = "exec ${menu}";
+
+              "${modifier}+${left}" = "focus left";
+              "${modifier}+${down}" = "focus down";
+              "${modifier}+${up}" = "focus up";
+              "${modifier}+${right}" = "focus right";
+
+              "${modifier}+Left" = "focus left";
+              "${modifier}+Down" = "focus down";
+              "${modifier}+Up" = "focus up";
+              "${modifier}+Right" = "focus right";
+
+              "${modifier}+Shift+${left}" = "move left";
+              "${modifier}+Shift+${down}" = "move down";
+              "${modifier}+Shift+${up}" = "move up";
+              "${modifier}+Shift+${right}" = "move right";
+
+              "${modifier}+Shift+Left" = "move left";
+              "${modifier}+Shift+Down" = "move down";
+              "${modifier}+Shift+Up" = "move up";
+              "${modifier}+Shift+Right" = "move right";
+
+              "${modifier}+b" = "splith";
+              "${modifier}+v" = "splitv";
+              "${modifier}+f" = "fullscreen toggle";
+              "${modifier}+a" = "focus parent";
+
+              "${modifier}+s" = "layout stacking";
+              "${modifier}+w" = "layout tabbed";
+              "${modifier}+e" = "layout toggle split";
+
+              "${modifier}+Shift+space" = "floating toggle";
+              "${modifier}+space" = "focus mode_toggle";
+
+              "${modifier}+1" = "workspace number 1";
+              "${modifier}+2" = "workspace number 2";
+              "${modifier}+3" = "workspace number 3";
+              "${modifier}+4" = "workspace number 4";
+              "${modifier}+5" = "workspace number 5";
+              "${modifier}+6" = "workspace number 6";
+              "${modifier}+7" = "workspace number 7";
+              "${modifier}+8" = "workspace number 8";
+              "${modifier}+9" = "workspace number 9";
+
+              "${modifier}+Shift+1" = "move container to workspace number 1";
+              "${modifier}+Shift+2" = "move container to workspace number 2";
+              "${modifier}+Shift+3" = "move container to workspace number 3";
+              "${modifier}+Shift+4" = "move container to workspace number 4";
+              "${modifier}+Shift+5" = "move container to workspace number 5";
+              "${modifier}+Shift+6" = "move container to workspace number 6";
+              "${modifier}+Shift+7" = "move container to workspace number 7";
+              "${modifier}+Shift+8" = "move container to workspace number 8";
+              "${modifier}+Shift+9" = "move container to workspace number 9";
+
+              "${modifier}+Shift+minus" = "move scratchpad";
+              "${modifier}+minus" = "scratchpad show";
+
+              "${modifier}+Shift+c" = "reload";
+              "${modifier}+Shift+e" = "exec swaynag -t warning -m 'You pressed the exit shortcut. Do you really want to exit sway? This will end your Wayland session.' -b 'Yes, exit sway' 'swaymsg exit'";
+
+              "${modifier}+r" = "mode resize";
+
+              # control volume
+              "XF86AudioMute" = "exec ${pkgs.pulseaudio}/bin/pactl set-sink-mute @DEFAULT_SINK@ toggle";
+              "XF86AudioRaiseVolume" = "exec ${pkgs.pulseaudio}/bin/pactl set-sink-volume @DEFAULT_SINK@ +5%";
+              "XF86AudioLowerVolume" = "exec ${pkgs.pulseaudio}/bin/pactl set-sink-volume @DEFAULT_SINK@ -5%";
+            }
+            # Thinkpad laptop
+            # -> needs some keybindings to control the hardware
+            // lib.optionalAttrs (config.networking.hostName == "daisy")
+              {
+                # control brightness
+                "XF86MonBrightnessUp" = "exec ${pkgs.light}/bin/light -A 10";
+                "XF86MonBrightnessDown" = "exec ${pkgs.light}/bin/light -U 10";
+              };
 
           # Brightness
           # bindsym XF86MonBrightnessDown exec light -U 10
