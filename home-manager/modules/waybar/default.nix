d59dc5511a53a8dfd98568e001066ba257edf4af
MayNiklas <info@niklas-steffen.de>
2023-07-21 11:35:25 +0200

    add waybar config

--- home-manager/modules/waybar/default.nix
+++ home-manager/modules/waybar/default.nix
@@ -9,6 +9,38 @@ in
 
     programs.waybar = {
       enable = true;
+      settings.mainbar = {
+        layer = "top";
+        position = "bottom";
+        spacing = 4; # Gaps between modules (4px)
+        modules-left = [ "sway/workspaces" "sway/mode" ];
+        modules-right = [ "tray" "network" "pulseaudio" "battery" "clock" ];
+        tray = {
+          spacing = 10;
+        };
+        network = {
+          format-wifi = "{essid} ({signalStrength}%)  ";
+          format-ethernet = "{ipaddr}/{cidr} ";
+          tooltip-format = "{ifname} via {gwaddr} ";
+          format-linked = "{ifname} (No IP) ";
+          format-disconnected = "Disconnected ⚠";
+          format-alt = "{ifname}: {ipaddr}/{cidr}";
+        };
+        pulseaudio = {
+          scroll-step = 1; # %, can be a float
+          format = "{volume}% {icon} {format_source}";
+          format-bluetooth = "{volume}% {icon} {format_source}";
+          format-bluetooth-muted = "ﱝ {icon} {format_source}";
+          format-muted = "ﱝ {format_source}";
+          format-source = "{volume}% ";
+          format-source-muted = "";
+          format-icons = {
+            headphone = "";
+            default = [ "" "" "" ];
+          };
+          on-click = "${pkgs.pavucontrol}/bin/pavucontrol";
+        };
+      };
     };
 
   };
