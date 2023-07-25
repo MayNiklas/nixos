0763212892036461871a7424cb4845cf1cc70344
MayNiklas <info@niklas-steffen.de>
2023-07-25 18:25:40 +0200

    move wlay into overlay

--- overlays/default.nix
+++ overlays/default.nix
@@ -31,29 +31,10 @@ self: super: {
     unifiLTS unifi5 unifi6 unifi7;
   unifi = super.pkgs.unifi7;
 
-  # # override with newer version from nixpkgs-unstable
-  # # tautulli = self.unstable.tautulli;
-
-  # # override with newer version from nixpkgs-unstable (home-manager related)
-  # chromium = self.unstable.chromium;
-  # discord = self.unstable.discord;
-  # firefox = self.unstable.firefox;
-  # jackett = self.unstable.pkgs.jackett;
-  # jetbrains.jdk = self.unstable.jetbrains.jdk;
-  # jetbrains.clion = self.unstable.jetbrains.clion;
-  # jetbrains.idea-ultimate = self.unstable.jetbrains.idea-ultimate;
-  # jetbrains.pycharm-professional = self.unstable.jetbrains.pycharm-professional;
-  # # neovim-unwrapped = self.unstable.neovim-unwrapped;
-  # obs-studio = self.unstable.obs-studio;
-  # signal-desktop = self.unstable.signal-desktop;
-  # sonarr = self.unstable.sonarr;
-  # spotify = self.unstable.spotify;
-  # sublime-merge = self.unstable.sublime-merge;
-  # sublime3 = self.unstable.sublime3;
-  # teamspeak_client = self.unstable.teamspeak_client;
-  # tdesktop = self.unstable.tdesktop;
-  # thunderbird-bin = self.unstable.thunderbird-bin;
-  # vscode = self.unstable.vscode;
-  # youtube-dl = self.unstable.youtube-dl;
-  # zoom-us = self.unstable.zoom-us;
+  # override with newer version from nixpkgs-unstable
+  wlay = self.unstable.wlay;
+  discord = self.unstable.discord;
+  firefox = self.unstable.firefox;
+  spotify = self.unstable.spotify;
+  zoom-us = self.unstable.zoom-us;
 }
