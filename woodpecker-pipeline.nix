19fa72456658879bce82ead0b3e93cf61c97c84e
MayNiklas <info@niklas-steffen.de>
2023-08-28 10:53:58 +0200

    fix pipeline

--- woodpecker-pipeline.nix
+++ woodpecker-pipeline.nix
@@ -1,44 +1,51 @@
-{ pkgs, flake-self, inputs }:
+{ pkgs, lib, flake-self, inputs }:
 with pkgs;
 writeText "pipeline" (builtins.toJSON {
-  configs = let
-    atticSetupStep = {
-      name = "Setup Attic";
-      image = "bash";
-      commands = [
-        "attic login lounge-rocks https://cache.lounge.rocks $ATTIC_KEY --set-default"
-      ];
-      secrets = [ "attic_key" ];
-    };
-    atticPushStep = {
-      name = "Push to Attic";
-      image = "bash";
-      commands = [ "attic push lounge-rocks:nix-cache result" ];
-      secrets = [ "attic_key" ];
-    };
-  in [
-    # TODO Show flake info
-  ] ++
+  configs =
+    let
+      # Map platform names between woodpecker and nix
+      woodpecker-platforms = {
+        "aarch64-linux" = "linux/arm64";
+        "x86_64-linux" = "linux/amd64";
+      };
+      atticSetupStep = {
+        name = "Setup Attic";
+        image = "bash";
+        commands = [
+          "attic login lounge-rocks https://cache.lounge.rocks $ATTIC_KEY --set-default"
+        ];
+        secrets = [ "attic_key" ];
+      };
+      atticPushStep = {
+        name = "Push to Attic";
+        image = "bash";
+        commands = [ "attic push lounge-rocks:nix-cache result" ];
+        secrets = [ "attic_key" ];
+      };
+    in
+    [
+      # TODO Show flake info
+    ] ++
 
-  # Hosts
-  map (host: {
-    name = "Host: ${host}";
-    data = (builtins.toJSON {
-      labels.backend = "local";
-      platform = lib.mkIf
-        (flake-self.nixosConfigurations.${host}.config.nixpkgs.system
-          == "x86_64-linux") "linux/amd64";
-      pipeline = [
-        atticSetupStep
-        {
-          name = "Build configuration for ${host}";
-          image = "bash";
-          commands = [
-            "nix build '.#nixosConfigurations.${host}.config.system.build.toplevel'"
+    # Hosts
+    map
+      (host: {
+        name = "Host: ${host}";
+        data = (builtins.toJSON {
+          labels.backend = "local";
+          platform = woodpecker-platforms."${flake-self.nixosConfigurations.${host}.config.nixpkgs.system}";
+          pipeline = [
+            atticSetupStep
+            {
+              name = "Build configuration for ${host}";
+              image = "bash";
+              commands = [
+                "nix build '.#nixosConfigurations.${host}.config.system.build.toplevel'"
+              ];
+            }
+            atticPushStep
           ];
-        }
-        atticPushStep
-      ];
-    });
-  }) (builtins.attrNames flake-self.nixosConfigurations);
+        });
+      })
+      (builtins.attrNames flake-self.nixosConfigurations);
 })
