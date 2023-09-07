f181250e4f10443964ca594626e6a2b3c3e1fae9
MayNiklas <info@niklas-steffen.de>
2023-09-07 12:23:18 +0200

    improve pipelines

--- woodpecker-pipeline.nix
+++ woodpecker-pipeline.nix
@@ -9,6 +9,16 @@ writeText "pipeline" (builtins.toJSON {
         "aarch64-linux" = "linux/arm64";
         "x86_64-linux" = "linux/amd64";
       };
+      nixFlakeShow = {
+        name = "Nix flake show";
+        image = "bash";
+        commands = [ "nix flake show" ];
+      };
+      nixFlakeCheck = {
+        name = "Nix flake check";
+        image = "bash";
+        commands = [ "nix flake check" ];
+      };
       atticSetupStep = {
         name = "Setup Attic";
         image = "bash";
@@ -18,39 +28,6 @@ writeText "pipeline" (builtins.toJSON {
         secrets = [ "attic_key" ];
       };
     in
-    [
-      {
-        name = "Nix flake check";
-        data = (builtins.toJSON {
-          labels.backend = "local";
-          platform = "linux/amd64";
-          steps = [
-            {
-              name = "nix flake show";
-              image = "bash";
-              commands = [
-                "nix flake show"
-              ];
-            }
-            {
-              name = "nix flake info";
-              image = "bash";
-              commands = [
-                "nix flake info"
-              ];
-            }
-            {
-              name = "nix flake check";
-              image = "bash";
-              commands = [
-                "nix flake check"
-              ];
-            }
-          ];
-        });
-      }
-    ] ++
-    # Hosts
     pkgs.lib.lists.flatten ([
       (map
         (arch: {
@@ -59,28 +36,32 @@ writeText "pipeline" (builtins.toJSON {
             labels.backend = "local";
             # platform will be deprecated in the future!
             platform = woodpecker-platforms."${arch}";
-            steps = pkgs.lib.lists.flatten ([ atticSetupStep ] ++ (map
-              (host:
-                if
-                # Skip hosts with this option set
-                  flake-self.nixosConfigurations.${host}.config.mayniklas.defaults.CISkip then
-                  [ ]
-                else if
-                # only build hosts for the arch we are currently building
-                  (flake-self.nixosConfigurations.${host}.pkgs.stdenv.hostPlatform.system != arch) then
-                  [ ]
-                else [
-                  {
-                    name = "Build configuration for ${host}";
-                    image = "bash";
-                    commands = [
-                      "nix build '.#nixosConfigurations.${host}.config.system.build.toplevel' -o 'result-${host}'"
-                      "attic push lounge-rocks:nix-cache 'result-${host}'"
-                    ];
-                  }
-                ])
-              (builtins.attrNames flake-self.nixosConfigurations)));
+            steps = pkgs.lib.lists.flatten
+              ([ nixFlakeShow nixFlakeCheck atticSetupStep ] ++ (map
+                (host:
+                  if
+                  # Skip hosts with this option set
+                    flake-self.nixosConfigurations.${host}.config.mayniklas.defaults.CISkip then
+                    [ ]
+                  else if
+                  # only build hosts for the arch we are currently building
+                    (flake-self.nixosConfigurations.${host}.pkgs.stdenv.hostPlatform.system
+                      != arch) then
+                    [ ]
+                  else [
+                    {
+                      name = "Build ${host}";
+                      image = "bash";
+                      commands = [ "nix build '.#nixosConfigurations.${host}.config.system.build.toplevel' -o 'result-${host}'" ];
+                    }
+                    {
+                      name = "Push ${host} to Attic";
+                      image = "bash";
+                      commands = [ "attic push lounge-rocks:nix-cache 'result-${host}'" ];
+                    }
+                  ])
+                (builtins.attrNames flake-self.nixosConfigurations)));
           });
-        }) [ "x86_64-linux" "aarch64-linux" ])
+        }) [ "x86_64-linux" ])
     ]);
 })
