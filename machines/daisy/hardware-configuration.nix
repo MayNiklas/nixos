5c650d18eea072956824e2d760aedd72a7dd6bb6
MayNiklas <info@niklas-steffen.de>
2023-09-27 14:44:58 +0200

    daisy always on performance

--- machines/daisy/hardware-configuration.nix
+++ machines/daisy/hardware-configuration.nix
@@ -46,6 +46,6 @@
   # networking.interfaces.wwan0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
+  powerManagement.cpuFreqGovernor = lib.mkDefault "performance";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
