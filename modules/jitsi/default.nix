e02f5a6f3b3b1efb643140e79f2838efdac232a6
MayNiklas <info@niklas-steffen.de>
2023-11-12 11:27:37 +0100

    improve on jitsi module

--- modules/jitsi/default.nix
+++ modules/jitsi/default.nix
@@ -17,29 +17,39 @@ in
 
   config = mkIf cfg.enable {
 
-    services.jitsi-videobridge.openFirewall = true;
-    services.jitsi-meet = {
-      enable = true;
-      hostName = "${cfg.hostname}";
-
-      jicofo.enable = true;
-      nginx.enable = true;
-
-      config = { defaultLang = "en"; };
-
-      interfaceConfig = {
-        SHOW_JITSI_WATERMARK = false;
-        SHOW_WATERMARK_FOR_GUESTS = false;
-      };
-
-    };
-
-    networking.firewall = {
-      allowedTCPPorts = [ 80 443 ];
-      allowedUDPPorts = [ 10000 ];
-    };
-
+    # enable common NGINX settings
     mayniklas.nginx.enable = true;
+
+    services = {
+      jitsi-videobridge.openFirewall = true;
+      jitsi-meet = {
+        enable = true;
+        hostName = "${cfg.hostname}";
+        config = {
+          defaultLang = "en";
+          enableWelcomePage = true;
+          requireDisplayName = true;
+          analytics.disabled = true;
+          startAudioOnly = true;
+          channelLastN = 4;
+          stunServers = [
+            { urls = "turn:turn.matrix.org:3478?transport=udp"; }
+            { urls = "turn:turn.matrix.org:3478?transport=tcp"; }
+          ];
+          constraints.video.height = {
+            ideal = 720;
+            max = 1080;
+            min = 240;
+          };
+        };
+        interfaceConfig = {
+          SHOW_JITSI_WATERMARK = false;
+          SHOW_WATERMARK_FOR_GUESTS = false;
+          DISABLE_PRESENCE_STATUS = true;
+          GENERATE_ROOMNAMES_ON_WELCOME_PAGE = false;
+        };
+      };
+    };
 
   };
 }
