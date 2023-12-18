534e448138678d77f80317b0d9b1d0343c83b921
MayNiklas <info@niklas-steffen.de>
2023-12-18 12:46:35 +0100

    add gitSync

--- home-manager/modules/zsh/default.nix
+++ home-manager/modules/zsh/default.nix
@@ -68,6 +68,13 @@ in
         # git status
         gs = "${pkgs.git}/bin/git status";
 
+        # git sync
+        gitSync = ''
+          ${pkgs.git}/bin/git fetch upstream
+          ${pkgs.git}/bin/git checkout main
+          ${pkgs.git}/bin/git merge upstream/main
+        '';
+
         # clean up repository
         clean = "${pkgs.git}/bin/git clean -xdn";
         destroy = "${pkgs.git}/bin/git clean -xdf";
