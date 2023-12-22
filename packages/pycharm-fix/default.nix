6f4a9de7f22f2cffd38b8c7f0540085e60f2eda1
MayNiklas <info@niklas-steffen.de>
2023-12-22 21:17:33 +0100

    add print

--- packages/pycharm-fix/default.nix
+++ packages/pycharm-fix/default.nix
@@ -2,6 +2,7 @@
 pkgs.writeShellScriptBin "pycharm-fix" ''
   for f in ~/.local/share/JetBrains/*; do
     if [[ $f == *"PyCharm"* ]]; then
+      echo "Fixing PyCharm at $f/copilot-agent-linux..."
       ln -s ${github-copilot-intellij-agent}/bin/copilot-agent $f/copilot-agent-linux
     fi
   done
