88e583c63c64875492680b2666c585164bd8fed4
MayNiklas <info@niklas-steffen.de>
2023-12-23 12:35:32 +0100

    use copilot 1.4.5.4049

--- packages/pycharm-fix/default.nix
+++ packages/pycharm-fix/default.nix
@@ -1,9 +1,20 @@
-{ writeShellScriptBin, nix, github-copilot-intellij-agent, ... }:
+{ writeShellScriptBin, nix, github-copilot-intellij-agent, fetchurl, ... }:
+let
+  my-github-copilot-intellij-agent = github-copilot-intellij-agent.overrideAttrs (oldAttrs: rec {
+    pname = "github-copilot-intellij-agent";
+    version = "1.4.5.4049";
+    src = fetchurl {
+      name = "${pname}-${version}-plugin.zip";
+      url = "https://plugins.jetbrains.com/plugin/download?updateId=454005";
+      hash = "sha256-ibu3OcmtyLHuumhJQ6QipsNEIdEhvLUS7sb3xmnaR0U=";
+    };
+  });
+in
 writeShellScriptBin "pycharm-fix" ''
   for f in ~/.local/share/JetBrains/*; do
     if [[ $f == *"PyCharm"* ]]; then
-      ${nix}/bin/nix-store --add-root $f/copilot-agent-linux -r ${github-copilot-intellij-agent}/bin/copilot-agent
-      echo "Created link from $f/copilot-agent-linux to ${github-copilot-intellij-agent}/bin/copilot-agent..."
+      ${nix}/bin/nix-store --add-root $f/copilot-agent-linux -r ${my-github-copilot-intellij-agent}/bin/copilot-agent
+      echo "Created link from $f/copilot-agent-linux to ${my-github-copilot-intellij-agent}/bin/copilot-agent..."
     fi
   done
 ''
