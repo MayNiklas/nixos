1f1d943bcc012d4d8069552183c2f82736fcb7a8
MayNiklas <info@niklas-steffen.de>
2023-12-25 09:01:18 +0100

    daisy: powersave

--- machines/daisy/configuration.nix
+++ machines/daisy/configuration.nix
@@ -1,13 +1,10 @@
 # TTY: Control + Alt + F1
 # nix run github:numtide/nixos-anywhere -- --flake .#daisy root@192.168.5.61
-{ config, lib, pkgs, nixos-hardware, ... }: {
+{ config, lib, pkgs, ... }: {
 
   imports = [
     # Include the results of the hardware scan.
     ./hardware-configuration.nix
-
-    # https://github.com/NixOS/nixos-hardware/tree/master/lenovo/thinkpad/x390
-    nixos-hardware.nixosModules.lenovo-thinkpad-x390
   ];
 
   home-manager.users."${config.mayniklas.home-manager.username}" = {
