b1f8e092693ada90c216913bb9f2973daf8ce04a
ALinkBetweenNets <20593829+ALinkbetweenNets@users.noreply.github.com>
2024-08-03 14:40:16 +0200

    changed vscode nix language server from nil to nixd

--- home-manager/modules/vscode/default.nix
+++ home-manager/modules/vscode/default.nix
@@ -1,6 +1,12 @@
-{ lib, pkgs, config, ... }:
+{
+  lib,
+  pkgs,
+  config,
+  ...
+}:
 with lib;
-let cfg = config.mayniklas.programs.vscode;
+let
+  cfg = config.mayniklas.programs.vscode;
 in
 {
   options.mayniklas.programs.vscode.enable = mkEnableOption "enable vscode";
@@ -42,14 +48,37 @@ in
         };
 
         # jnoortheen.nix-ide
-        "nix" = {
-          "enableLanguageServer" = true;
-          "serverPath" = "nil";
-          "serverSettings" = {
-            "nil" = {
-              "formatting" = {
-                "command" = [ "nixpkgs-fmt" ];
+        "[nix]" = {
+          "editor.defaultFormatter" = "jnoortheen.nix-ide";
+        };
+        "nix.enableLanguageServer" = true;
+        # "serverPath" = "${pkgs.nil}/bin/nil";
+        "nix.serverPath" = "${pkgs.nixd}/bin/nixd";
+        "nix.serverSettings" = {
+          # "nil" = {
+          #   "diagnostics" = {
+          #     "ignored" = [ "unused_binding" "unused_with" ];
+          #   };
+          #   "formatting" = {
+          #     "command" = [ "${pkgs.nixpkgs-fmt}/bin/nixpkgs-fmt" ];
+          #   };
+          # };
+          "nixd" = {
+            # "eval" = { };
+            "formatting" = {
+              "command" = [ "${pkgs.nixfmt-rfc-style}/bin/nixfmt" ];
+            };
+            "options" = {
+              "nixos" = {
+                "expr" = "(builtins.getFlake \"/home/l/nix\").nixosConfigurations.fn.options";
               };
+              "home-manager" = {
+                "expr" = "(builtins.getFlake \"/home/l/nix\").homeConfigurations.laptop.options";
+              };
+              # "target" = {
+              #   "args" = [ ];
+              #   "installable" = "(builtins.getFlake \"\${workspaceFolder}\")#nixosConfigurations.<name>.options";
+              # };
             };
           };
         };
@@ -66,24 +95,27 @@ in
         "go.toolsManagement.checkForUpdates" = "off";
       };
 
-      extensions = with pkgs.vscode-extensions; [
-        davidanson.vscode-markdownlint
-        github.copilot
-        github.github-vscode-theme
-        github.vscode-github-actions
-        github.vscode-pull-request-github
-        golang.go
-        james-yu.latex-workshop
-        jnoortheen.nix-ide
-        ms-azuretools.vscode-docker
-        ms-python.python
-        ms-vscode-remote.remote-ssh
-        ms-vscode.cpptools
-        redhat.vscode-xml
-        redhat.vscode-yaml
-        rust-lang.rust-analyzer
-        yzhang.markdown-all-in-one
-      ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [ ];
+      extensions =
+        with pkgs.vscode-extensions;
+        [
+          davidanson.vscode-markdownlint
+          github.copilot
+          github.github-vscode-theme
+          github.vscode-github-actions
+          github.vscode-pull-request-github
+          golang.go
+          james-yu.latex-workshop
+          jnoortheen.nix-ide
+          ms-azuretools.vscode-docker
+          ms-python.python
+          ms-vscode-remote.remote-ssh
+          ms-vscode.cpptools
+          redhat.vscode-xml
+          redhat.vscode-yaml
+          rust-lang.rust-analyzer
+          yzhang.markdown-all-in-one
+        ]
+        ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [ ];
 
     };
   };
