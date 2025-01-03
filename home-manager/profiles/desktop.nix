2d3d401f12145e4b6a0688ffedbb5bd0962c1940
MayNiklas <info@niklas-steffen.de>
2025-01-03 17:06:27 +0100

    disable ansible

--- home-manager/profiles/desktop.nix
+++ home-manager/profiles/desktop.nix
@@ -62,7 +62,7 @@
   mayniklas = {
     programs = {
       alacritty.enable = true;
-      ansible.enable = true;
+      # currently broken ansible.enable = true;
       chromium.enable = true;
       devolopment.enable = true;
       direnv.enable = true;
