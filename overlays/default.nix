0c68edb06d0828ccdca1c8cc85ebeb48f189910f
MayNiklas <info@niklas-steffen.de>
2025-02-16 22:01:03 +0100

    add system to overlay

--- overlays/default.nix
+++ overlays/default.nix
@@ -3,8 +3,9 @@ let
   # Pass flake inputs to overlay so we can use the sources pinned in flake.lock
   # instead of having to keep sha256 hashes in each package for src
   inherit inputs;
-in
-self: super: {
+in self: super:
+let system = super.system;
+in {
   # Custom packages. Will be made available on all machines and used where
   # needed.
 
