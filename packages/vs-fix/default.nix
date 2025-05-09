7bd79979dce9b12cb7fcc63a241f32f5261f6aa0
Niklas <44636701+MayNiklas@users.noreply.github.com>
2025-05-09 16:05:09 +0200

    migrate to pkgs.nodejs_20 (untested)

--- packages/vs-fix/default.nix
+++ packages/vs-fix/default.nix
@@ -3,9 +3,9 @@ let
   vs-fix-skript = pkgs.writeShellScriptBin "vs-fix" ''
     for f in ~/.vscode-server/bin/*; do
       rm $f/node            
-      ln -s $(which ${pkgs.nodejs-18_x}/bin/node) $f/node
+      ln -s $(which ${pkgs.nodejs_20}/bin/node) $f/node
     done
-    ${pkgs.nix}/bin/nix-store --add-root ~/.vscode-server/.keep-node -r ${pkgs.nodejs-18_x}
+    ${pkgs.nix}/bin/nix-store --add-root ~/.vscode-server/.keep-node -r ${pkgs.nodejs_20}
     echo "Done patching vs-code server!"
   '';
 in
