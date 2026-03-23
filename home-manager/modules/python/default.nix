6f76700edd6919374195fcf7d411278720954d72
MayNiklas <info@niklas-steffen.de>
2026-03-23 09:07:28 +0100

    add python deps

--- home-manager/modules/python/default.nix
+++ home-manager/modules/python/default.nix
@@ -1,20 +1,66 @@
-{ lib, pkgs, config, ... }:
+{
+  lib,
+  pkgs,
+  config,
+  ...
+}:
 with lib;
 let
   cfg = config.mayniklas.programs.python;
-  my-python-packages = python-packages:
-    with python-packages; [
-      requests
-    ];
-  python-with-packages = pkgs.python3.withPackages my-python-packages;
-
 in
 {
 
   options.mayniklas.programs.python.enable = mkEnableOption "enable python3 with libs";
 
   config = mkIf cfg.enable {
-    home.packages = with pkgs; [ python-with-packages ];
+    home.packages =
+      let
+        my-python-packages =
+          python-packages: with python-packages; [
+            requestsaiohttp
+            beautifulsoup4
+            black
+            dateparser
+            flake8
+            fpdf2
+            httpx
+            jsonschema
+            lxml
+            matplotlib
+            nltk
+            numpy
+            ollama
+            openai
+            opencv-python
+            openpyxl
+            pandas
+            pillow
+            pip
+            plotly
+            psutil
+            pydantic
+            pypdf2
+            pytest
+            python-dateutil
+            python-dotenv
+            pyyaml
+            reportlab
+            requests
+            scikit-learn
+            seaborn
+            setuptools
+            # spacy
+            # tensorflow
+            # textblob
+            toml
+            torch
+            transformers
+            weasyprint
+          ];
+        python-with-packages = pkgs.python3.withPackages my-python-packages;
+      in
+      with pkgs;
+      [ python-with-packages ];
   };
 
 }
