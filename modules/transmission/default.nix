a3aa41e31064f9a38834521944ffb3c7754f1aeb
MayNiklas <info@niklas-steffen.de>
2026-03-23 09:47:05 +0100

    fix: use pkgs.stdenv.hostPlatform.system instead of deprecated pkgs.system in transmission module

--- modules/transmission/default.nix
+++ modules/transmission/default.nix
@@ -6,7 +6,7 @@ let
     url =
       "https://github.com/NixOS/nixpkgs/archive/0c19708cf035f50d28eb4b2b8e7a79d4dc52f6bb.tar.gz";
     sha256 = "sha256:0ngw2shvl24swam5pzhcs9hvbwrgzsbcdlhpvzqc7nfk8lc28sp3";
-  }) { system = "${pkgs.system}"; };
+  }) { system = pkgs.stdenv.hostPlatform.system; };
 in {
 
   options.mayniklas.transmission = {
