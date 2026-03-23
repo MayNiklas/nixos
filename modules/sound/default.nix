2adf9eb380bf1602b8e7ef560d362fc190ee2113
MayNiklas <info@niklas-steffen.de>
2026-03-23 09:39:43 +0100

    fix: use services.pulseaudio instead of deprecated hardware.pulseaudio

--- modules/sound/default.nix
+++ modules/sound/default.nix
@@ -12,7 +12,7 @@ in
     # The option definition `sound' no longer has any effect; please remove it.
     # The option was heavily overloaded and can be removed from most configurations.
     # sound.enable = true;
-    hardware.pulseaudio.enable = false;
+    services.pulseaudio.enable = false;
     security.rtkit.enable = true;
     services.pipewire = {
       enable = true;
